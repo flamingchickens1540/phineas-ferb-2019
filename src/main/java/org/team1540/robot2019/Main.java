dedfad5972b4172a0abffe39c396090a0ac94e68
Zachary Robinson <robinsonz@catlin.edu>
2019-01-06 22:16:11 -0800

    Create base robot and drivetrain

---
+++ src/main/java/org/team1540/robot2019/Main.java
@@ -0,0 +1,9 @@
+package org.team1540.robot2019;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Main {
+  public static void main(String[] args) {
+    RobotBase.startRobot(Robot::new);
+  }
+}
---
+++ src/main/java/org/team1540/robot2019/RobotMap.java
@@ -0,0 +1,11 @@
+package org.team1540.robot2019;
+
+public class RobotMap {
+  public static final int DRIVE_LEFT_A = 1;
+  public static final int DRIVE_LEFT_B = 2;
+  public static final int DRIVE_LEFT_C = 3;
+
+  public static final int DRIVE_RIGHT_A = 4;
+  public static final int DRIVE_RIGHT_B = 5;
+  public static final int DRIVE_RIGHT_C = 6;
+}
