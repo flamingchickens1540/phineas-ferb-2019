a530182d2184eb194befb18ee293e57fda1bd761
Zachary Robinson <robinsonz@catlin.edu>
2019-01-12 23:00:46 -0800

    Add basic actuators and sensors
    This is after our robot design meeting where we figured out the robot
    construction.

--- src/main/java/org/team1540/robot2019/RobotMap.java
+++ src/main/java/org/team1540/robot2019/RobotMap.java
@@ -1,6 +1,8 @@
 package org.team1540.robot2019;
 
 public class RobotMap {
+
+  // motors
   public static final int DRIVE_LEFT_A = 1;
   public static final int DRIVE_LEFT_B = 2;
   public static final int DRIVE_LEFT_C = 3;
@@ -8,4 +10,28 @@ public class RobotMap {
   public static final int DRIVE_RIGHT_A = 4;
   public static final int DRIVE_RIGHT_B = 5;
   public static final int DRIVE_RIGHT_C = 6;
+
+  public static final int ELEVATOR_A = 7;
+  public static final int ELEVATOR_B = 8;
+
+  public static final int INTAKE_TOP = 9;
+  public static final int INTAKE_BTM = 10;
+
+
+  // pneumatics
+  public static final int ELEVATOR_BRAKE = 0;
+
+  public static final int ARM_ACTUATOR = 0;
+
+  public static final int HATCH_ACTUATOR = 0;
+
+  public static final int CLIMBER_REAR_PISTON = 0;
+
+  // sensors and switches
+  public static final int ELEVATOR_TOP_SW = 0;
+  public static final int ELEVATOR_BTM_SW = 0;
+
+  public static final int INTAKE_SENSOR = 0;
+
+  public static final int GROUND_PROXIMITY_SENSOR = 0;
 }
