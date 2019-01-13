e9182d2b9df9109e483b4e0923ac5c1aa6a2bc17
Zachary Robinson <robinsonz@catlin.edu>
2019-01-13 12:59:01 -0800

    Add subsystems
    They don't 100% exist yet (climber and hatch mechanisms aren't here)
    because I haven't coordinated with those people yet. Also, the intake
    sensor type isn't locked in yet (might be analog, might be digital,
    etc).

--- src/main/java/org/team1540/robot2019/Robot.java
+++ src/main/java/org/team1540/robot2019/Robot.java
@@ -2,11 +2,18 @@ package org.team1540.robot2019;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import org.team1540.robot2019.subsystems.Arm;
 import org.team1540.robot2019.subsystems.Drivetrain;
+import org.team1540.robot2019.subsystems.Elevator;
+import org.team1540.robot2019.subsystems.Intake;
 import org.team1540.rooster.preferencemanager.PreferenceManager;
 
 public class Robot extends TimedRobot {
-  public static final Drivetrain drivetrain = new Drivetrain();
+
+  public static Drivetrain drivetrain;
+  public static Elevator elevator;
+  public static Arm arm;
+  public static Intake intake;
 
   @Override
   public void robotInit() {
@@ -14,6 +21,14 @@ public class Robot extends TimedRobot {
 
     OI.init();
 
+    Scheduler.getInstance().run();
+
+    // initialize them after we run the scheduler once so that the preference manager can update its
+    // values
+    drivetrain = new Drivetrain();
+    elevator = new Elevator();
+    arm = new Arm();
+    intake = new Intake();
 
     // TODO: shuffleboard
   }
