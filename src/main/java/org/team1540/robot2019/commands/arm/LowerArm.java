dbc98ad7b8c3d0a5db0507a9e4df008b2ee0d4f6
Zachary Robinson <robinsonz@catlin.edu>
2019-01-13 16:47:05 -0800

    Create arm commands

---
+++ src/main/java/org/team1540/robot2019/commands/arm/LowerArm.java
@@ -0,0 +1,8 @@
+package org.team1540.robot2019.commands.arm;
+
+public class LowerArm extends MoveArm {
+
+  public LowerArm() {
+    super(false);
+  }
+}
---
+++ src/main/java/org/team1540/robot2019/commands/arm/RaiseArm.java
@@ -0,0 +1,8 @@
+package org.team1540.robot2019.commands.arm;
+
+public class RaiseArm extends MoveArm {
+
+  public RaiseArm() {
+    super(true);
+  }
+}
