14461d3696a71e478da167ed4f684b96de33339c
ambercmerrill <ambercmerrill@gmail.com>
2019-01-26 21:24:51 -0800

    Add climber leg methods

--- src/main/java/org/team1540/robot2019/subsystems/Climber.java
+++ src/main/java/org/team1540/robot2019/subsystems/Climber.java
@@ -4,10 +4,19 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.team1540.robot2019.commands.climber.JoystickClimberArms;
 
-import static org.team1540.robot2019.Hardware.climberArmLeft;
-import static org.team1540.robot2019.Hardware.climberArmRight;
+import static org.team1540.robot2019.Hardware.*;
 
 public class Climber extends Subsystem {
+
+    public void legsDown() {
+        climberCylinder1.set(true);
+        climberCylinder2.set(false);
+    }
+
+    public void legsUp() {
+        climberCylinder1.set(false);
+        climberCylinder2.set(true);
+    }
 
     public void joystickArms(double value) {
         climberArmLeft.set(ControlMode.PercentOutput, value);
