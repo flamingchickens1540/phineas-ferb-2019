60ba10c3e6df5ff573529674edf092bdbf611e1e
Zachary Robinson <robinsonz@catlin.edu>
2019-01-27 13:27:52 -0800

    Move tuner robot classes to separate package

--- src/main/java/org/team1540/robot2019/tuners/ElevatorTuningRobot.java
+++ src/main/java/org/team1540/robot2019/tuners/ElevatorTuningRobot.java
@@ -1,8 +1,9 @@
-package org.team1540.robot2019;
+package org.team1540.robot2019.tuners;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import org.team1540.robot2019.Hardware;
 import org.team1540.robot2019.subsystems.*;
 import org.team1540.rooster.Utilities;
 
