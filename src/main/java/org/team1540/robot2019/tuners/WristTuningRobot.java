878519356b42a6ccc80f24c13d836dff2c989d86
Zachary Robinson <robinsonz@catlin.edu>
2019-01-27 22:29:41 -0800

    Create WristTuningRobot

---
+++ src/main/java/org/team1540/robot2019/tuners/WristTuningRobot.java
@@ -0,0 +1,33 @@
+package org.team1540.robot2019.tuners;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import org.team1540.robot2019.Hardware;
+import org.team1540.robot2019.subsystems.*;
+import org.team1540.rooster.Utilities;
+
+public class WristTuningRobot extends TimedRobot {
+
+  private static Wrist wrist;
+  private Joystick joystick = new Joystick(0);
+
+  @Override
+  public void robotInit() {
+    Hardware.initWrist();
+
+    wrist = new Wrist();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    wrist.set(Utilities.processDeadzone(joystick.getRawAxis(1), 0.1));
+
+    wrist.setCylinder(joystick.getRawButton(1));
+  }
+
+  @Override
+  public void robotPeriodic() {
+    Scheduler.getInstance().run();
+  }
+}
