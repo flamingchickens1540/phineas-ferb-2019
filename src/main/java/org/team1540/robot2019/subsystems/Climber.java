2ad1646d9b2a215d5417633b82912c7d372d1014
ambercmerrill <ambercmerrill@gmail.com>
2019-01-28 19:00:22 -0800

    Make climber testing code actually work

--- src/main/java/org/team1540/robot2019/subsystems/Climber.java
+++ src/main/java/org/team1540/robot2019/subsystems/Climber.java
@@ -8,19 +8,27 @@ import static org.team1540.robot2019.Hardware.*;
 
 public class Climber extends Subsystem {
 
-    public void legsDown() {
+    public void cylinderDown() {
         climberCylinder1.set(true);
         climberCylinder2.set(false);
     }
 
-    public void legsUp() {
+    public void cylinderUp() {
         climberCylinder1.set(false);
         climberCylinder2.set(true);
     }
 
-    public void setRawArms(double value) {
-        climberArmLeft.set(ControlMode.PercentOutput, value);
-        climberArmRight.set(ControlMode.PercentOutput, value);
+    public void setArms(double value) {
+        climberArmLeft.set(ControlMode.PercentOutput, -value);
+//        climberArmRight.set(ControlMode.PercentOutput, value);
+    }
+
+    public double getCurrentLeft() {
+        return climberArmLeft.getOutputCurrent();
+    }
+
+    public double getCurrentRight() {
+        return climberArmRight.getOutputCurrent();
     }
 
     @Override
--- src/main/java/org/team1540/robot2019/tuners/ClimberTuningRobot.java
+++ src/main/java/org/team1540/robot2019/tuners/ClimberTuningRobot.java
@@ -4,32 +4,44 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.robot2019.Hardware;
 import org.team1540.robot2019.subsystems.Climber;
-import org.team1540.robot2019.subsystems.Wrist;
+import org.team1540.robot2019.subsystems.Elevator;
 import org.team1540.rooster.Utilities;
-import org.team1540.rooster.util.SimpleCommand;
 
 public class ClimberTuningRobot extends TimedRobot {
 
   private static Climber climber;
+  private static Elevator elevator;
   private Joystick joystick = new Joystick(0);
-  private JoystickButton button1 = new JoystickButton(joystick, 1);
-  private JoystickButton button2 = new JoystickButton(joystick, 2);
+  private Joystick joystick2 = new Joystick(1);
+//  private JoystickButton button1 = new JoystickButton(joystick, 1);
+//  private JoystickButton button2 = new JoystickButton(joystick, 2);
 
   @Override
   public void robotInit() {
-    Hardware.initWrist();
+    Hardware.initClimber();
+    Hardware.initElevator();
 
     climber = new Climber();
+    elevator = new Elevator();
+
+    elevator.setEnableController(false);
   }
 
   @Override
   public void teleopPeriodic() {
-    climber.setRawArms(Utilities.processDeadzone(joystick.getRawAxis(1), 0.1));
+    climber.setArms(Utilities.processDeadzone(joystick.getRawAxis(5), 0.1));
 
-    button1.whenPressed(new SimpleCommand("cylinder down", climber::legsDown));
-    button2.whenPressed(new SimpleCommand("cylinder up", climber::legsUp));
+    elevator.setRaw(Utilities.processDeadzone(joystick2.getRawAxis(1), 0.1) / 5);
+//    elevator.setRaw(0.04);
+
+    SmartDashboard.putNumber("climber current left", climber.getCurrentLeft());
+    SmartDashboard.putNumber("climber current right", climber.getCurrentRight());
+
+//    button1.whenPressed(new SimpleCommand("cylinder down", climber::cylinderDown));
+//    button2.whenPressed(new SimpleCommand("cylinder up", climber::cylinderUp));
   }
 
   @Override
