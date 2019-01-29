c60baad0a46fb409aacd3a44b21c25c0ac57e496
Zachary Robinson <robinsonz@catlin.edu>
2019-01-29 10:37:52 -0800

    Add logging to Eject commannd

--- src/main/java/org/team1540/robot2019/commands/intake/Eject.java
+++ src/main/java/org/team1540/robot2019/commands/intake/Eject.java
@@ -1,10 +1,13 @@
 package org.team1540.robot2019.commands.intake;
 
 import edu.wpi.first.wpilibj.command.TimedCommand;
+import org.apache.log4j.Logger;
 import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.Tuning;
 
 public class Eject extends TimedCommand {
+
+  private static final Logger logger = Logger.getLogger(Eject.class);
 
   public Eject() {
     super(Tuning.intakeEjectTime);
@@ -13,11 +16,14 @@ public class Eject extends TimedCommand {
 
   @Override
   protected void initialize() {
+    logger.debug("Ejecting ball");
     Robot.intake.startEjecting();
   }
 
   @Override
   protected void end() {
+    logger.debug("Stopped ejecting");
+
     Robot.intake.stop();
   }
 }
