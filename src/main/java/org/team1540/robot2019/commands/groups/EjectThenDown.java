2a2687065ba17dd0e960def4d9897e2de148dcee
Zachary Robinson <robinsonz@catlin.edu>
2019-01-29 10:46:14 -0800

    Rename gamepieceGroups package
    pAcKaGeS aReN't cAmElCaSe

--- src/main/java/org/team1540/robot2019/commands/groups/EjectThenDown.java
+++ src/main/java/org/team1540/robot2019/commands/groups/EjectThenDown.java
@@ -1,4 +1,4 @@
-package org.team1540.robot2019.commands.gamepieceGroups;
+package org.team1540.robot2019.commands.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.Tuning;
