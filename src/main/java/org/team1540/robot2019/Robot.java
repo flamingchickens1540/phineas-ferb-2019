e2fcc4138266f50f5cb01b2de8a34bd818903931
Zachary Robinson <robinsonz@catlin.edu>
2019-02-01 09:15:41 -0800

    Add some shuffleboard recording

--- src/main/java/org/team1540/robot2019/Robot.java
+++ src/main/java/org/team1540/robot2019/Robot.java
@@ -1,10 +1,12 @@
 package org.team1540.robot2019;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.shuffleboard.EventImportance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.apache.log4j.Level;
@@ -28,6 +30,8 @@ public class Robot extends TimedRobot {
   public static Intake intake;
   public static HatchMech hatchMech;
   public static Climber climber;
+
+  boolean disableBrakes;
 
   @Override
   public void robotInit() {
@@ -77,18 +81,29 @@ public class Robot extends TimedRobot {
     logger.debug("Disabling drive brakes in 2 seconds...");
     brakeTimer.reset();
     brakeTimer.start();
+    disableBrakes = true;
 
     wrist.handleDisable();
+
+    if (DriverStation.getInstance().isFMSAttached()) {
+      logger.debug("FMS is attached, auto-stopping recording");
+      Shuffleboard.stopRecording();
+    }
+
+    Shuffleboard.addEventMarker("Robot Disable", EventImportance.kNormal);
 
     Hardware.checkStickyFaults();
   }
 
   @Override
   public void disabledPeriodic() {
-    if (brakeTimer.hasPeriodPassed(2)) {
+    if (brakeTimer.hasPeriodPassed(2) && disableBrakes) {
       brakeTimer.stop();
       drivetrain.setBrake(false);
       logger.debug("Drive brakes disabled");
+      disableBrakes = false;
+
+      Shuffleboard.addEventMarker("Drive brakes disabled", EventImportance.kTrivial);
     }
   }
 
@@ -97,6 +112,18 @@ public class Robot extends TimedRobot {
     drivetrain.setBrake(true);
 
     Hardware.checkStickyFaults();
+
+    if (DriverStation.getInstance().isFMSAttached()) {
+      logger.debug("FMS is attached, auto-starting recording");
+      Shuffleboard.setRecordingFileNameFormat(
+          DriverStation.getInstance().getEventName() + "-" + DriverStation.getInstance()
+              .getMatchType() + "-" + DriverStation.getInstance().getMatchNumber()
+              + "-${date}-${time}");
+
+      Shuffleboard.startRecording();
+    }
+
+    Shuffleboard.addEventMarker("Autonomous Start", EventImportance.kNormal);
   }
 
   @Override
@@ -108,6 +135,12 @@ public class Robot extends TimedRobot {
     drivetrain.setBrake(true);
 
     Hardware.checkStickyFaults();
+
+    if (DriverStation.getInstance().isFMSAttached()) {
+      Shuffleboard.startRecording();
+    }
+
+    Shuffleboard.addEventMarker("Teleop Start", EventImportance.kNormal);
   }
 
   @Override
