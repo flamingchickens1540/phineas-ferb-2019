3d0023fd46c088df02a7d103273f42c0a570de5d
Zachary Robinson <robinsonz@catlin.edu>
2019-02-01 11:01:34 -0800

    Remove brake and ground proximity sensor

--- src/main/java/org/team1540/robot2019/RobotMap.java
+++ src/main/java/org/team1540/robot2019/RobotMap.java
@@ -23,8 +23,6 @@ public class RobotMap {
   public static final int CLIMBER_ARM_R = 5;
 
   // pneumatics
-  public static final int ELEVATOR_BRAKE = 0;
-
   public static final int WRIST_CYLINDER_1 = 0;
 
   public static final int HATCH_SLIDE = 0;
@@ -40,8 +38,6 @@ public class RobotMap {
   public static final int ARM_BTM_SW = 3;
 
   public static final int INTAKE_SENSOR = 0;
-
-  public static final int GROUND_PROXIMITY_SENSOR = 0;
 
   public static final int PRESSURE_SENSOR = 0;
 }
