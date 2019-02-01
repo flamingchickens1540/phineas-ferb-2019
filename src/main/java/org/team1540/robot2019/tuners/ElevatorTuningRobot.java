38d4af42d152bd8a10e8327d856a49161ea71489
Zachary Robinson <robinsonz@catlin.edu>
2019-02-01 08:30:55 -0800

    Retool ElevatorTuningRobot to test position PID

--- src/main/java/org/team1540/robot2019/tuners/ElevatorTuningRobot.java
+++ src/main/java/org/team1540/robot2019/tuners/ElevatorTuningRobot.java
@@ -2,28 +2,36 @@ package org.team1540.robot2019.tuners;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.team1540.robot2019.Hardware;
-import org.team1540.robot2019.subsystems.*;
-import org.team1540.rooster.Utilities;
+import org.team1540.robot2019.Robot;
+import org.team1540.robot2019.Tuning;
+import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
+import org.team1540.robot2019.subsystems.Elevator;
+import org.team1540.rooster.preferencemanager.PreferenceManager;
 
 public class ElevatorTuningRobot extends TimedRobot {
 
-  private static Elevator elevator;
   private Joystick joystick = new Joystick(0);
 
   @Override
   public void robotInit() {
+    Logger.getRootLogger().setLevel(Level.DEBUG);
+    PreferenceManager.getInstance().add(new Tuning());
+    Scheduler.getInstance().run();
+
     Hardware.initElevator();
 
-    elevator = new Elevator();
+    Robot.elevator = new Elevator();
 
-    elevator.setEnableController(false);
-  }
+    Robot.elevator.setEnableController(false);
 
-  @Override
-  public void teleopPeriodic() {
-    elevator.setRaw(Utilities.processDeadzone(joystick.getRawAxis(1), 0.1));
+    new JoystickButton(joystick, 1).whenPressed(new MoveElevatorToPosition(0));
+    new JoystickButton(joystick, 2).whenPressed(new MoveElevatorToPosition(20));
+    new JoystickButton(joystick, 3).whenPressed(new MoveElevatorToPosition(28));
   }
 
   @Override
