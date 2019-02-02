8ff4a1cec0d954698a79a81fb73faefcc2f8bdb1
Zachary Robinson <robinsonz@catlin.edu>
2019-02-02 15:04:14 -0800

    Remove unused import

--- src/main/java/org/team1540/robot2019/commands/elevator/MoveElevatorToZero.java
+++ src/main/java/org/team1540/robot2019/commands/elevator/MoveElevatorToZero.java
@@ -1,11 +1,14 @@
 package org.team1540.robot2019.commands.elevator;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.team1540.robot2019.Robot;
+import org.team1540.rooster.util.SimpleCommand;
 
 public class MoveElevatorToZero extends CommandGroup {
 
   public MoveElevatorToZero() {
     addSequential(new MoveElevatorToPosition(0));
-    addSequential(new ZeroElevator());
+//    addSequential(new ZeroElevator());
+    addSequential(new SimpleCommand("dfsf", () -> Robot.elevator.setRaw(0), Robot.elevator));
   }
 }
