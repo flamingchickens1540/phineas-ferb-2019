5136bbdd6962f891c361336437602a9f1205ad38
ambercmerrill <ambercmerrill@gmail.com>
2019-02-01 20:16:42 -0800

    Add DoubleSolenoid for climber cylinder

--- src/main/java/org/team1540/robot2019/subsystems/Climber.java
+++ src/main/java/org/team1540/robot2019/subsystems/Climber.java
@@ -2,27 +2,29 @@ package org.team1540.robot2019.subsystems;
 
 import static org.team1540.robot2019.Hardware.climberArmLeft;
 import static org.team1540.robot2019.Hardware.climberArmRight;
-import static org.team1540.robot2019.Hardware.climberCylinder1;
-import static org.team1540.robot2019.Hardware.climberCylinder2;
+import static org.team1540.robot2019.Hardware.climberCylinder;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Climber extends Subsystem {
 
   public void cylinderDown() {
-    climberCylinder1.set(true);
-    climberCylinder2.set(false);
+    climberCylinder.set(DoubleSolenoid.Value.kForward);
   }
 
   public void cylinderUp() {
-    climberCylinder1.set(false);
-    climberCylinder2.set(true);
+    climberCylinder.set(DoubleSolenoid.Value.kReverse);
   }
 
   public void setArms(double value) {
     climberArmLeft.set(ControlMode.PercentOutput, -value);
 //        climberArmRight.set(ControlMode.PercentOutput, value);
+  }
+
+  public void setArmsConstant(double speed) {
+
   }
 
   public double getCurrentLeft() {
