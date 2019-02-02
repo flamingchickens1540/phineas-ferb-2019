7e73b128f3acc92c0932c46bba69320a1deb7105
wangl5 <wangl5@users.noreply.github.com>
2019-02-01 20:27:49 -0800

    Fix some issues with UDP auto lineup (WIP)

--- src/main/java/org/team1540/robot2019/vision/commands/UDPAutoLineup.java
+++ src/main/java/org/team1540/robot2019/vision/commands/UDPAutoLineup.java
@@ -6,10 +6,15 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.team1540.robot2019.Hardware;
 import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.Tuning;
 import org.team1540.robot2019.datastructures.threed.Transform3D;
 import org.team1540.robot2019.datastructures.twod.Twist2D;
+import org.team1540.robot2019.tuners.AutoAlignTuningRobot;
+import org.team1540.robot2019.utils.LimelightLocalization;
+import org.team1540.robot2019.utils.TankDriveOdometry;
+import org.team1540.robot2019.utils.TankDriveOdometryRunnable;
 import org.team1540.robot2019.utils.TankDriveTwist2DInput;
 import org.team1540.robot2019.utils.TrigUtils;
 import org.team1540.rooster.drive.pipeline.CTREOutput;
@@ -19,18 +24,24 @@ import org.team1540.rooster.functional.Executable;
 import org.team1540.rooster.wrappers.RevBlinken.ColorPattern;
 
 public class UDPAutoLineup extends Command {
+
+  private final LimelightLocalization limeLoc;
+  private final TankDriveOdometryRunnable driveOdometry;
+
   Transform3D goal;
   private Executable pipeline;
   private TankDriveTwist2DInput twist2DInput;
 
-  public UDPAutoLineup() {
+  public UDPAutoLineup(LimelightLocalization limeLoc, TankDriveOdometryRunnable driveOdometry) {
+    this.limeLoc = limeLoc;
+    this.driveOdometry = driveOdometry;
     requires(Robot.drivetrain);
     twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadius);
     pipeline = twist2DInput
         .then(new FeedForwardProcessor(0.27667, 0.054083,0.08694))
         // .then((Processor<TankDriveData, TankDriveData>) tankDriveData -> new TankDriveData(tankDriveData.left, tankDriveData.right))
         .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
-        .then(new CTREOutput(Robot.drivetrain.driveLeftMotorA, Robot.drivetrain.driveRightMotorA, true));
+        .then(Robot.drivetrain.getPipelineOutput());
   }
 
   @Override
@@ -38,7 +49,6 @@ public class UDPAutoLineup extends Command {
     Robot.drivetrain.reset();
     Robot.drivetrain.configTalonsForVelocity();
 
-    Robot.leds.set(ColorPattern.CHASE_BLUE);
 
     NetworkTable tebConfigTable = NetworkTableInstance.getDefault().getTable("TEBPlanner/Config");
     tebConfigTable.getEntry("TEBReset").setBoolean(true);
@@ -47,13 +57,13 @@ public class UDPAutoLineup extends Command {
     tebConfigTable.getEntry("AccLimX").setNumber(0.7);
     tebConfigTable.getEntry("MaxVelTheta").setNumber(6.0);
     tebConfigTable.getEntry("AccLimTheta").setNumber(7.0);
-    if (Robot.limelightLocalization.attemptUpdatePose()) { // TODO: Make this distance tunable
+    if (limeLoc.attemptUpdatePose()) { // TODO: Make this distance tunable
       computeAndUpdateGoal();
     } else {
-      if (Robot.limelightLocalization.millisSinceLastAcquired() < 2000) {
+      if (limeLoc.millisSinceLastAcquired() < 2000) {
         updateGoal(Robot.lastOdomToLimelight);
       }
-      Robot.leds.set(ColorPattern.RED);
+//      Robot.leds.set(ColorPattern.RED);
       cancel();
     }
   }
@@ -63,8 +73,8 @@ public class UDPAutoLineup extends Command {
   }
 
   private Transform3D computeGoal() {
-    return Robot.wheelOdometry.getOdomToBaseLink()
-        .add(Robot.limelightLocalization.getBaseLinkToVisionTarget())
+    return driveOdometry.getOdomToBaseLink()
+        .add(limeLoc.getBaseLinkToVisionTarget())
         .add(new Transform3D(new Vector3D(-0.65, 0, 0), Rotation.IDENTITY));
   }
 
@@ -79,7 +89,7 @@ public class UDPAutoLineup extends Command {
 
   @Override
   protected void execute() {
-    if (Robot.limelightLocalization.attemptUpdatePose() && (getDistanceError() > 0.07)) { // TODO: Make this distance tunable
+    if (limeLoc.attemptUpdatePose() && (getDistanceError() > 0.07)) { // TODO: Make this distance tunable
       computeAndUpdateGoal();
     }
 
@@ -96,14 +106,14 @@ public class UDPAutoLineup extends Command {
     }
     if (getDistanceError() < 0.02 && Math.abs(getAngleError()) < Math.toRadians(2)) {
       Robot.drivetrain.stop();
-      Robot.leds.set(ColorPattern.LIME);
+//      Robot.leds.set(ColorPattern.LIME);
       return true;
     }
     return false;
   }
 
   private double getDistanceError() {
-    Vector3D odomPosition = Robot.wheelOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
+    Vector3D odomPosition = driveOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
     return goal.toTransform2D().getPositionVector().distance(new Vector2D(odomPosition.getX(), odomPosition.getY()));
   }
 
