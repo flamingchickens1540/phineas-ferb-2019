d4896947a518cf2052efac9a381d1914d901ee28
ambercmerrill <ambercmerrill@gmail.com>
2019-02-04 16:17:47 -0800

    Update hatch mech control to match new mech

--- src/main/java/org/team1540/robot2019/subsystems/HatchMech.java
+++ src/main/java/org/team1540/robot2019/subsystems/HatchMech.java
@@ -1,5 +1,6 @@
 package org.team1540.robot2019.subsystems;
 
+import static org.team1540.robot2019.Hardware.hatchGrabber;
 import static org.team1540.robot2019.Hardware.hatchSlide;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -25,9 +26,11 @@ public class HatchMech extends Subsystem {
   }
 
   public void attatch() {
+    hatchGrabber.set(true);
   }
 
   public void release() {
+    hatchGrabber.set(false);
   }
 
   @Override
