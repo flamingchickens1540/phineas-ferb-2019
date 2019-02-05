7e9003faa80acf3f4fa464285d7a2a455c3eeb15
Zachary Robinson <robinsonz@catlin.edu>
2019-02-04 20:25:26 -0800

    Add gyro-based climber code

--- src/main/java/org/team1540/robot2019/tuners/ClimberTuningRobot.java
+++ src/main/java/org/team1540/robot2019/tuners/ClimberTuningRobot.java
@@ -4,70 +4,44 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.robot2019.Hardware;
 import org.team1540.robot2019.Robot;
-import org.team1540.robot2019.Tuning;
-import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2019.subsystems.Climber;
-import org.team1540.robot2019.subsystems.Drivetrain;
-import org.team1540.robot2019.subsystems.Elevator;
-import org.team1540.rooster.Utilities;
+import org.team1540.robot2019.commands.climber.RaiseUpGyroAssist;
+import org.team1540.robot2019.commands.groups.PrepareForClimb;
 import org.team1540.rooster.preferencemanager.PreferenceManager;
 import org.team1540.rooster.util.SimpleCommand;
 
 public class ClimberTuningRobot extends TimedRobot {
 
-  private static Climber climber;
   private Joystick joystick = new Joystick(0);
-  private JoystickButton buttonA = new JoystickButton(joystick, 1);
-  private JoystickButton buttonB = new JoystickButton(joystick, 2);
-  private JoystickButton buttonX = new JoystickButton(joystick, 3);
-  private JoystickButton buttonY = new JoystickButton(joystick, 4);
-  private JoystickButton buttonSt = new JoystickButton(joystick, 8);
-
+  private JoystickButton button1 = new JoystickButton(joystick, 1);
+  private JoystickButton button2 = new JoystickButton(joystick, 2);
+  private JoystickButton button3 = new JoystickButton(joystick, 3);
+  private JoystickButton button4 = new JoystickButton(joystick, 4);
 
   @Override
   public void robotInit() {
-    Hardware.initClimber();
-    Hardware.initElevator();
-    Hardware.initDrive();
-
-    climber = new Climber();
-    Robot.elevator = new Elevator();
-    Robot.drivetrain = new Drivetrain();
-
     PreferenceManager.getInstance().add(new Tuning());
+
     Scheduler.getInstance().run();
 
-    new MoveElevatorToPosition(Tuning.elevatorDownPosition).start();
-  }
+    Hardware.initClimber();
+    Hardware.initNavX();
+    Hardware.initElevator();
 
-  @Override
-  public void teleopInit() {
-//    new MoveElevatorToPosition(Tuning.elevatorDownPosition).start();
+    Robot.climber = new Climber();
+
+    button1.whenPressed(new RaiseUpGyroAssist());
+    button2.whenPressed(new PrepareForClimb());
+    button3
+        .whenPressed(new SimpleCommand("Raise Cylinder", Robot.climber::cylinderUp, Robot.climber));
+    button4.whenPressed(new SimpleCommand("stahp", () -> Robot.climber.setArms(0), Robot.climber));
   }
 
   @Override
   public void teleopPeriodic() {
-    climber.setArms(-Utilities.processDeadzone(joystick.getRawAxis(5), 0.1));
 
-    double drive = -Utilities.processDeadzone(joystick.getRawAxis(2), 0.1);
-    Robot.drivetrain.setThrottle(drive, drive);
-
-//    elevator.setRaw(Utilities.processDeadzone(joystick2.getRawAxis(1), 0.1) / 5);
-//    elevator.setRaw(0.04);
-
-    SmartDashboard.putNumber("climber current left", climber.getCurrentLeft());
-    SmartDashboard.putNumber("climber current right", climber.getCurrentRight());
-
-//    buttonA.whenPressed(new MoveElevatorToPosition(Tuning.elevatorUpPosition));
-    buttonA.whenPressed(new SimpleCommand("start climbing", climber::startClimbing));
-    buttonB.whenPressed(new SimpleCommand("retract cylinder", climber::onPlatform));
-
-    buttonX.whenPressed(new MoveElevatorToPosition(Tuning.elevatorUpPosition));
-    buttonY.whenPressed(new MoveElevatorToPosition(Tuning.elevatorDownPosition));
-//    buttonSt.whenPressed(new SimpleCommand("arms back", climber::backward));
   }
 
   @Override
