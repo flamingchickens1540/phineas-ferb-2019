fe520de420b086a3edb747d1ad34062bb33afde1
Zachary Robinson <robinsonz@catlin.edu>
2019-02-06 10:01:35 -0800

    Add drivetrain to climber robot

--- src/main/java/org/team1540/robot2019/tuners/ClimberTuningRobot.java
+++ src/main/java/org/team1540/robot2019/tuners/ClimberTuningRobot.java
@@ -6,16 +6,19 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import org.team1540.robot2019.Hardware;
 import org.team1540.robot2019.Robot;
+import org.team1540.robot2019.ShuffleboardDisplay;
 import org.team1540.robot2019.Tuning;
 import org.team1540.robot2019.commands.climber.RaiseUpGyroAssist;
 import org.team1540.robot2019.commands.groups.PrepareForClimb;
 import org.team1540.robot2019.subsystems.Climber;
+import org.team1540.robot2019.subsystems.Drivetrain;
+import org.team1540.robot2019.subsystems.Elevator;
 import org.team1540.rooster.preferencemanager.PreferenceManager;
 import org.team1540.rooster.util.SimpleCommand;
 
 public class ClimberTuningRobot extends TimedRobot {
 
-  private Joystick joystick = new Joystick(0);
+  private Joystick joystick = new Joystick(1);
   private JoystickButton button1 = new JoystickButton(joystick, 1);
   private JoystickButton button2 = new JoystickButton(joystick, 2);
   private JoystickButton button3 = new JoystickButton(joystick, 3);
@@ -30,14 +33,20 @@ public class ClimberTuningRobot extends TimedRobot {
     Hardware.initClimber();
     Hardware.initNavX();
     Hardware.initElevator();
+    Hardware.initDrive();
+    Hardware.initPressureSensor();
+
+    ShuffleboardDisplay.init();
 
     Robot.climber = new Climber();
+    Robot.elevator = new Elevator();
+    Robot.drivetrain = new Drivetrain();
 
     button1.whenPressed(new RaiseUpGyroAssist());
     button2.whenPressed(new PrepareForClimb());
     button3
         .whenPressed(new SimpleCommand("Raise Cylinder", Robot.climber::cylinderUp, Robot.climber));
-    button4.whenPressed(new SimpleCommand("stahp", () -> Robot.climber.setArms(0), Robot.climber));
+    button3.whenPressed(new SimpleCommand("stahp", () -> Robot.climber.setArms(0), Robot.climber));
   }
 
   @Override
