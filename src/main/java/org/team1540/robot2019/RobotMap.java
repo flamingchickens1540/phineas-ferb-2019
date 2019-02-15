587118720e8bb39684dd7d894934b41d23c9c90a
Zachary Robinson <robinsonz@catlin.edu>
2019-02-14 22:43:51 -0800

    Correct RobotMap and inversions
    These will need to change for Ferb

--- src/main/java/org/team1540/robot2019/RobotMap.java
+++ src/main/java/org/team1540/robot2019/RobotMap.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.SPI.Port;
 public class RobotMap {
 
   // motors
-  public static final int DRIVE_LEFT_A = 11;
+  public static final int DRIVE_LEFT_A = 13;
   public static final int DRIVE_LEFT_B = 12;
-  public static final int DRIVE_LEFT_C = 13;
+  public static final int DRIVE_LEFT_C = 11;
 
   public static final int DRIVE_RIGHT_A = 1;
   public static final int DRIVE_RIGHT_B = 2;
@@ -16,20 +16,20 @@ public class RobotMap {
   public static final int ELEVATOR_L = 6;
   public static final int ELEVATOR_R = 7;
 
-  public static final int INTAKE_WRIST = 9;
+  public static final int INTAKE_WRIST = 8;
 
-  public static final int INTAKE_TOP = 8;
+  public static final int INTAKE_TOP = 9;
   public static final int INTAKE_BTM = 10;
 
-  public static final int CLIMBER_ARM_L = 4;
-  public static final int CLIMBER_ARM_R = 5;
+  public static final int CLIMBER_ARM_L = 5;
+  public static final int CLIMBER_ARM_R = 4;
 
   // pneumatics
 
-  public static final int CLIMBER_CYLINDER_1 = 6;
-  public static final int CLIMBER_CYLINDER_2 = 7;
+  public static final int CLIMBER_CYLINDER_1 = 2;
+  public static final int CLIMBER_CYLINDER_2 = 3;
 
-  public static final int HATCH_SLIDE = 2;
+  public static final int HATCH_SLIDE = 1;
   public static final int HATCH_GRABBER = 0;
 
   // sensors and switches
@@ -44,12 +44,12 @@ public class RobotMap {
 
 
   // pdp ports
-  public static final int PDP_DRIVE_LEFT_A = 0;
-  public static final int PDP_DRIVE_LEFT_B = 0;
-  public static final int PDP_DRIVE_LEFT_C = 0;
+  public static final int PDP_DRIVE_LEFT_A = 15;
+  public static final int PDP_DRIVE_LEFT_B = 14;
+  public static final int PDP_DRIVE_LEFT_C = 13;
   public static final int PDP_DRIVE_RIGHT_A = 0;
-  public static final int PDP_DRIVE_RIGHT_B = 0;
-  public static final int PDP_DRIVE_RIGHT_C = 0;
+  public static final int PDP_DRIVE_RIGHT_B = 2;
+  public static final int PDP_DRIVE_RIGHT_C = 1;
   public static final int PDP_INTAKE_WRIST = 0;
   public static final int PDP_INTAKE_TOP = 0;
   public static final int PDP_INTAKE_BTM = 0;
