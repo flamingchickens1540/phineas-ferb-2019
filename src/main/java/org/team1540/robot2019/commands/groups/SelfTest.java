4ccd6f3b8faed8321a04d26f789f54be77f1c427
Zachary Robinson <robinsonz@catlin.edu>
2019-02-15 14:34:55 -0800

    Create hatch self-test

--- src/main/java/org/team1540/robot2019/commands/groups/SelfTest.java
+++ src/main/java/org/team1540/robot2019/commands/groups/SelfTest.java
@@ -3,6 +3,7 @@ package org.team1540.robot2019.commands.groups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.apache.log4j.Logger;
 import org.team1540.robot2019.commands.elevator.ElevatorSelfTest;
+import org.team1540.robot2019.commands.hatch.HatchSelfTest;
 import org.team1540.robot2019.commands.intake.IntakeSelfTest;
 import org.team1540.robot2019.commands.wrist.WristSelfTest;
 import org.team1540.rooster.util.SimpleCommand;
@@ -13,6 +14,7 @@ public class SelfTest extends CommandGroup {
 
   public SelfTest() {
     addSequential(new SimpleCommand("Print status", () -> logger.info("Starting robot self-test")));
+    addSequential(new HatchSelfTest());
     addSequential(new IntakeSelfTest());
     addSequential(new WristSelfTest());
     addSequential(new ElevatorSelfTest());
