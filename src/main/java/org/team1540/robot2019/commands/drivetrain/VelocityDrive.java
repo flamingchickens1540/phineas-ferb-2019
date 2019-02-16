d90d0e89fd54b52ff68dc56aa0c431d148ea3f40
wangl5 <wangl5@users.noreply.github.com>
2019-02-15 22:42:35 -0800

    Make VelocityDrive not require a drivetrain argument

--- src/main/java/org/team1540/robot2019/commands/drivetrain/VelocityDrive.java
+++ src/main/java/org/team1540/robot2019/commands/drivetrain/VelocityDrive.java
@@ -4,40 +4,35 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.robot2019.Hardware;
 import org.team1540.robot2019.OI;
+import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.Tuning;
-import org.team1540.robot2019.subsystems.Drivetrain;
 
 public class VelocityDrive extends Command {
 
-  private final Drivetrain dt;
+    public VelocityDrive() {
+        requires(Robot.drivetrain);
+    }
 
-  public VelocityDrive(Drivetrain dt) {
-    this.dt = dt;
-    requires(dt);
-//    dt.reset();
-    dt.configTalonsForVelocity();
+    @Override
+    protected void initialize() {
+        Robot.drivetrain.configTalonsForVelocity();
   }
 
   @Override
   protected void execute() {
-//    double triggerValue = OI.getTankdriveForwardsAxis() - OI.getTankdriveBackwardsAxis();
-//    double leftSetpoint = (triggerValue - OI.getTankdriveLeftAxis()) * Tuning.drivetrainMaxVelocity;
-//    double rightSetpoint = (triggerValue - OI.getTankdriveRightAxis()) * Tuning.drivetrainMaxVelocity;
-//    dt.setLeftVelocity(leftSetpoint);
-//    dt.setRightVelocity(rightSetpoint);
     double triggerValue = OI.getTankdriveForwardsAxis() - OI.getTankdriveBackwardsAxis();
     double leftSetpoint = (triggerValue - OI.getTankdriveLeftAxis()) * Tuning.drivetrainMaxVelocity;
     double rightSetpoint = (triggerValue - OI.getTankdriveRightAxis()) * Tuning.drivetrainMaxVelocity;
-    dt.setLeftVelocity(leftSetpoint);
-    dt.setRightVelocity(rightSetpoint);
+      Robot.drivetrain.setLeftVelocity(leftSetpoint);
+      Robot.drivetrain.setRightVelocity(rightSetpoint);
       SmartDashboard.putNumber("debug-setpoint-left", leftSetpoint * 10 / Tuning.drivetrainTicksPerMeter);
       SmartDashboard.putNumber("debug-setpoint-right", rightSetpoint * 10 / Tuning.drivetrainTicksPerMeter);
     SmartDashboard.putNumber("debug-setpoint-left-raw", leftSetpoint);
     SmartDashboard.putNumber("debug-setpoint-right-raw", rightSetpoint);
-    SmartDashboard.putNumber("debug-velocity-left", dt.getLeftVelocityMetersPerSecond());
-    SmartDashboard.putNumber("debug-velocity-right", dt.getRightVelocityMetersPerSecond());
-    SmartDashboard.putNumber("debug-velocity-left-raw", dt.getLeftVelocity());
-    SmartDashboard.putNumber("debug-velocity-right-raw", dt.getRightVelocity());
+      SmartDashboard.putNumber("debug-velocity-left", Robot.drivetrain.getLeftVelocityMetersPerSecond());
+      SmartDashboard.putNumber("debug-velocity-right", Robot.drivetrain.getRightVelocityMetersPerSecond());
+      SmartDashboard.putNumber("debug-velocity-left-raw", Robot.drivetrain.getLeftVelocity());
+      SmartDashboard.putNumber("debug-velocity-right-raw", Robot.drivetrain.getRightVelocity());
     SmartDashboard.putNumber("debug-velocity-right-error", Hardware.driveRightMotorA.getClosedLoopError());
     SmartDashboard.putNumber("debug-velocity-left-error", Hardware.driveLeftMotorA.getClosedLoopError());
   }
