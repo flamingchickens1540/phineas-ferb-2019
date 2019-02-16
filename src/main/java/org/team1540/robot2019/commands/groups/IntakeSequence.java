43333437afb98268c513a4170d346906271cd6f5
wangl5 <wangl5@users.noreply.github.com>
2019-02-16 13:45:45 -0800

    Slide in during intake sequence

--- src/main/java/org/team1540/robot2019/commands/groups/IntakeSequence.java
+++ src/main/java/org/team1540/robot2019/commands/groups/IntakeSequence.java
@@ -1,13 +1,16 @@
 package org.team1540.robot2019.commands.groups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
 import org.team1540.robot2019.commands.intake.IntakeAuto;
 import org.team1540.robot2019.commands.wrist.LowerWrist;
+import org.team1540.rooster.util.SimpleCommand;
 
 public class IntakeSequence extends CommandGroup {
 
   public IntakeSequence() {
+      addParallel(new SimpleCommand("alaska's", Robot.hatchMech::slideIn, Robot.hatchMech));
     addSequential(new MoveElevatorToZero());
     addSequential(new LowerWrist());
     addSequential(new IntakeAuto());
