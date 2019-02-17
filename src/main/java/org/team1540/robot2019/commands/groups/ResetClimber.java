57e51470f477e7494e8cee8bb4765bc323894fa9
Zachary Robinson <robinsonz@catlin.edu>
2019-02-17 12:10:36 -0800

    Move elevator to clear before moving arms back

--- src/main/java/org/team1540/robot2019/commands/groups/ResetClimber.java
+++ src/main/java/org/team1540/robot2019/commands/groups/ResetClimber.java
@@ -3,11 +3,13 @@ package org.team1540.robot2019.commands.groups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.Tuning;
 import org.team1540.robot2019.commands.climber.MoveArmsToPosition;
+import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
 
 public class ResetClimber extends CommandGroup {
 
   public ResetClimber() {
+    addSequential(new MoveElevatorToPosition(Tuning.elevatorClimbPosition));
     addSequential(new MoveArmsToPosition(Tuning.climberBackPos));
     addSequential(new MoveElevatorToZero());
   }
