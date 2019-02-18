6a7619f311d1f85253f700aa9fac72ffc6b9bbaa
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-02-17 16:56:35 -0800

    Rename GetHatch to PrepGetHatch

--- src/main/java/org/team1540/robot2019/commands/hatch/PrepGetHatch.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/PrepGetHatch.java
@@ -4,9 +4,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.Robot;
 import org.team1540.rooster.util.SimpleCommand;
 
-public class GetHatch extends CommandGroup {
+public class PrepGetHatch extends CommandGroup {
 
-  public GetHatch() {
+    public PrepGetHatch() {
       addSequential(new SimpleCommand("Hatch Slide Out", Robot.hatchMech::slideOut, Robot.hatchMech));
 //    addSequential(new GrabHatch());
 //    addSequential(new WaitCommand(0.3));
