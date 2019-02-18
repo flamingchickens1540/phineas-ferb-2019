5159b4d3ba8c6f68ab3ab453ae4ce4f08515a860
wangl5 <wangl5@users.noreply.github.com>
2019-02-17 22:49:07 -0800

    Rename drive commands

--- src/main/java/org/team1540/robot2019/vision/commands/PurePursuitThenPoint.java
+++ src/main/java/org/team1540/robot2019/vision/commands/PurePursuitThenPoint.java
@@ -2,6 +2,7 @@ package org.team1540.robot2019.vision.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.Robot;
+import org.team1540.robot2019.commands.drivetrain.DriveForTimeVelocity;
 
 public class PurePursuitThenPoint extends CommandGroup {
 
@@ -11,7 +12,7 @@ public class PurePursuitThenPoint extends CommandGroup {
         addSequential(new PointLineupDumb());
 //        addSequential(new PointLineup(Robot.limelightLocalization, Robot.wheelOdometry, Robot.lastOdomToVisionTarget));
 
-        addSequential(new DriveVelocityForTime(0.4, 0.5));
+        addSequential(new DriveForTimeVelocity(0.4, 0.5));
     }
 
 }
