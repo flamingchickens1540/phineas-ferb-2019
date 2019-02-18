b0e92d139c0902a2b4cd2fcb1a36314b8120d9ac
wangl5 <wangl5@users.noreply.github.com>
2019-02-17 22:46:45 -0800

    Reformat code

--- src/main/java/org/team1540/robot2019/utils/DualVisionTargetLocalizationUtils.java
+++ src/main/java/org/team1540/robot2019/utils/DualVisionTargetLocalizationUtils.java
@@ -12,75 +12,75 @@ import org.team1540.robot2019.datastructures.threed.Transform3D;
 
 public class DualVisionTargetLocalizationUtils {
 
-  private static double CM_TOLERANCE = 0.0001;
+    private static double CM_TOLERANCE = 0.0001;
 
-  public static Vector2D anglesFromScreenSpace(Vector2D normalizedScreenPoint, double hoz_fov, double vert_fov) {
-    //http://docs.limelightvision.io/en/latest/theory.html#from-pixels-to-angles
-    double vpw = 2.0 * Math.tan(hoz_fov / 2);
-    double vph = 2.0 * Math.tan(vert_fov / 2);
+    public static Vector2D anglesFromScreenSpace(Vector2D normalizedScreenPoint, double hoz_fov, double vert_fov) {
+        //http://docs.limelightvision.io/en/latest/theory.html#from-pixels-to-angles
+        double vpw = 2.0 * Math.tan(hoz_fov / 2);
+        double vph = 2.0 * Math.tan(vert_fov / 2);
 
-    double screenSpaceX = vpw / 2.0
-        * -normalizedScreenPoint.getX(); // X is negated TODO: Move this negation elsewhere
-    double screenSpaceY = vph / 2.0 * normalizedScreenPoint.getY();
+        double screenSpaceX = vpw / 2.0
+            * -normalizedScreenPoint.getX(); // X is negated TODO: Move this negation elsewhere
+        double screenSpaceY = vph / 2.0 * normalizedScreenPoint.getY();
 
-    return new Vector2D(
-        Math.PI / 2 - Math.atan2(1, screenSpaceX),
-        Math.PI / 2 - Math.atan2(1, screenSpaceY)
-    );
-  }
-
-  public static Line lineFromScreenAngles(Vector2D screenAngles, Vector3D cameraPosition, Rotation cameraRotation) {
-    double yaw = screenAngles.getX();
-    double pitch = screenAngles.getY();
-
-    Vector3D pixelVector = Vector3D.PLUS_I;
-
-    Rotation pitchRot = new Rotation(Vector3D.PLUS_J, pitch, RotationConvention.FRAME_TRANSFORM);
-    Rotation yawRot = new Rotation(Vector3D.PLUS_K, yaw, RotationConvention.FRAME_TRANSFORM);
-
-    pixelVector = pitchRot.applyTo(pixelVector);
-    pixelVector = yawRot.applyTo(pixelVector);
-
-    Vector3D pixelVectorRotated = cameraRotation.applyTo(pixelVector);
-
-    return new Line(cameraPosition, cameraPosition.add(pixelVectorRotated), CM_TOLERANCE);
-  }
-
-  public static Vector3D getIntersection(Line line, double height) {
-    return new Plane(Vector3D.PLUS_K.scalarMultiply(height), Vector3D.PLUS_K, CM_TOLERANCE)
-        .intersection(line);
-  }
-
-  public static Vector3D midpoint(Vector3D a, Vector3D b) {
-    return new Vector3D(
-        (a.getX() + b.getX()) / 2,
-        (a.getY() + b.getY()) / 2,
-        (a.getZ() + b.getZ()) / 2
-    );
-  }
-
-  public static double angleFromVisionTargets(Vector2D left, Vector2D right) {
-    Vector2D difference = left.subtract(right);
-    double atan = Math.atan(difference.getY() / difference.getX())+Math.PI/2;
-    if (atan > Math.PI/2) {
-      atan = atan-Math.PI;
+        return new Vector2D(
+            Math.PI / 2 - Math.atan2(1, screenSpaceX),
+            Math.PI / 2 - Math.atan2(1, screenSpaceY)
+        );
     }
-    return atan;
-  }
 
-  private static Vector2D xyFromVector3D(Vector3D vec) {
-    return new Vector2D(vec.getX(), vec.getY());
-  }
+    public static Line lineFromScreenAngles(Vector2D screenAngles, Vector3D cameraPosition, Rotation cameraRotation) {
+        double yaw = screenAngles.getX();
+        double pitch = screenAngles.getY();
 
-  public static Transform3D poseFromTwoCamPoints(Vector2D leftAngles, Vector2D rightAngles, double planeHeight, Vector3D cameraPosition, Rotation cameraRotation, double hoz_fov, double vert_fov) {
+        Vector3D pixelVector = Vector3D.PLUS_I;
 
-    Vector3D leftPoint = getIntersection(lineFromScreenAngles(anglesFromScreenSpace(leftAngles, hoz_fov, vert_fov), cameraPosition, cameraRotation), planeHeight);
-    Vector3D rightPoint = getIntersection(lineFromScreenAngles(anglesFromScreenSpace(rightAngles, hoz_fov, vert_fov), cameraPosition, cameraRotation), planeHeight);
+        Rotation pitchRot = new Rotation(Vector3D.PLUS_J, pitch, RotationConvention.FRAME_TRANSFORM);
+        Rotation yawRot = new Rotation(Vector3D.PLUS_K, yaw, RotationConvention.FRAME_TRANSFORM);
 
-    return new Transform3D(
-        midpoint(leftPoint, rightPoint),
-        new Rotation(RotationOrder.XYZ, RotationConvention.FRAME_TRANSFORM, 0, 0, SmartDashboard.getNumber("angleMultiplier", 1) * angleFromVisionTargets(
-            xyFromVector3D(leftPoint),
-            xyFromVector3D(rightPoint))));
-  }
+        pixelVector = pitchRot.applyTo(pixelVector);
+        pixelVector = yawRot.applyTo(pixelVector);
+
+        Vector3D pixelVectorRotated = cameraRotation.applyTo(pixelVector);
+
+        return new Line(cameraPosition, cameraPosition.add(pixelVectorRotated), CM_TOLERANCE);
+    }
+
+    public static Vector3D getIntersection(Line line, double height) {
+        return new Plane(Vector3D.PLUS_K.scalarMultiply(height), Vector3D.PLUS_K, CM_TOLERANCE)
+            .intersection(line);
+    }
+
+    public static Vector3D midpoint(Vector3D a, Vector3D b) {
+        return new Vector3D(
+            (a.getX() + b.getX()) / 2,
+            (a.getY() + b.getY()) / 2,
+            (a.getZ() + b.getZ()) / 2
+        );
+    }
+
+    public static double angleFromVisionTargets(Vector2D left, Vector2D right) {
+        Vector2D difference = left.subtract(right);
+        double atan = Math.atan(difference.getY() / difference.getX()) + Math.PI / 2;
+        if (atan > Math.PI / 2) {
+            atan = atan - Math.PI;
+        }
+        return atan;
+    }
+
+    private static Vector2D xyFromVector3D(Vector3D vec) {
+        return new Vector2D(vec.getX(), vec.getY());
+    }
+
+    public static Transform3D poseFromTwoCamPoints(Vector2D leftAngles, Vector2D rightAngles, double planeHeight, Vector3D cameraPosition, Rotation cameraRotation, double hoz_fov, double vert_fov) {
+
+        Vector3D leftPoint = getIntersection(lineFromScreenAngles(anglesFromScreenSpace(leftAngles, hoz_fov, vert_fov), cameraPosition, cameraRotation), planeHeight);
+        Vector3D rightPoint = getIntersection(lineFromScreenAngles(anglesFromScreenSpace(rightAngles, hoz_fov, vert_fov), cameraPosition, cameraRotation), planeHeight);
+
+        return new Transform3D(
+            midpoint(leftPoint, rightPoint),
+            new Rotation(RotationOrder.XYZ, RotationConvention.FRAME_TRANSFORM, 0, 0, SmartDashboard.getNumber("angleMultiplier", 1) * angleFromVisionTargets(
+                xyFromVector3D(leftPoint),
+                xyFromVector3D(rightPoint))));
+    }
 }
--- src/main/java/org/team1540/robot2019/vision/commands/UDPAutoLineup.java
+++ src/main/java/org/team1540/robot2019/vision/commands/UDPAutoLineup.java
@@ -25,109 +25,109 @@ public class UDPAutoLineup extends Command {
 
     private final Drivetrain dt;
     private final UDPOdometryGoalSender sender;
-  private final UDPTwistReceiver receiver;
-  private final LimelightLocalization limeLoc;
-  private final TankDriveOdometryRunnable driveOdometry;
-  private final Transform3D lastOdomToLimelight;
-  private final AHRS navx;
+    private final UDPTwistReceiver receiver;
+    private final LimelightLocalization limeLoc;
+    private final TankDriveOdometryRunnable driveOdometry;
+    private final Transform3D lastOdomToLimelight;
+    private final AHRS navx;
 
-  Transform3D goal;
-  private Executable pipeline;
-  private TankDriveTwist2DInput twist2DInput;
+    Transform3D goal;
+    private Executable pipeline;
+    private TankDriveTwist2DInput twist2DInput;
 
-  public UDPAutoLineup(Drivetrain dt,UDPOdometryGoalSender sender, UDPTwistReceiver receiver, LimelightLocalization limeLoc, TankDriveOdometryRunnable driveOdometry, Transform3D lastOdomToLimelight, AHRS navx) {
-      this.dt = dt;
-      this.sender = sender;
-    this.receiver = receiver;
-    this.limeLoc = limeLoc;
-    this.driveOdometry = driveOdometry;
-    this.lastOdomToLimelight = lastOdomToLimelight;
-    this.navx = navx;
-    requires(dt);
-      twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadiusMeters);
-    pipeline = twist2DInput
-        .then(new FeedForwardProcessor(0, 0,0))
+    public UDPAutoLineup(Drivetrain dt, UDPOdometryGoalSender sender, UDPTwistReceiver receiver, LimelightLocalization limeLoc, TankDriveOdometryRunnable driveOdometry,
+        Transform3D lastOdomToLimelight, AHRS navx) {
+        this.dt = dt;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.limeLoc = limeLoc;
+        this.driveOdometry = driveOdometry;
+        this.lastOdomToLimelight = lastOdomToLimelight;
+        this.navx = navx;
+        requires(dt);
+        twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadiusMeters);
+        pipeline = twist2DInput
+            .then(new FeedForwardProcessor(0, 0, 0))
 //        .then(new FeedForwardProcessor(0.27667, 0.054083,0.08694))
-        // .then((Processor<TankDriveData, TankDriveData>) tankDriveData -> new TankDriveData(tankDriveData.left, tankDriveData.right))
-        .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
-        .then(dt.getPipelineOutput());
-  }
+            // .then((Processor<TankDriveData, TankDriveData>) tankDriveData -> new TankDriveData(tankDriveData.left, tankDriveData.right))
+            .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
+            .then(dt.getPipelineOutput());
+    }
 
-  @Override
-  protected void initialize() {
+    @Override
+    protected void initialize() {
 //    dt.reset();
-    dt.configTalonsForVelocity();
+        dt.configTalonsForVelocity();
 
-
-    NetworkTable tebConfigTable = NetworkTableInstance.getDefault().getTable("TEBPlanner/Config");
-    tebConfigTable.getEntry("TEBReset").setBoolean(true);
-      tebConfigTable.getEntry("MaxVelX").setNumber(1.5);
-      tebConfigTable.getEntry("MaxVelXBackwards").setNumber(1.4);
-      tebConfigTable.getEntry("AccLimX").setNumber(1.0);
-      tebConfigTable.getEntry("MaxVelTheta").setNumber(5.0);
-    tebConfigTable.getEntry("AccLimTheta").setNumber(12.0);
-    if (limeLoc.attemptUpdatePose()) { // TODO: Make this distance tunable
-      computeAndUpdateGoal();
-    } else {
-      if (limeLoc.millisSinceLastAcquired() < 10000) {
-        updateGoal(lastOdomToLimelight);
-      } else {
-  //      Robot.leds.set(ColorPattern.RED);
-        cancel();
-      }
-    }
-  }
-
-  private void computeAndUpdateGoal() {
-    updateGoal(computeGoal());
-  }
-
-  private Transform3D computeGoal() {
-    return driveOdometry.getOdomToBaseLink()
-        .add(limeLoc.getBaseLinkToVisionTarget())
-        .add(new Transform3D(new Vector3D(-0.65, -0.025, 0), Rotation.IDENTITY));
-  }
-
-  private void updateGoal(Transform3D newGoal) {
-    this.goal = newGoal;
-    sender.setGoal(goal.toTransform2D());
-    System.out.println("Goal updated");
-
-    Transform3D via_point = goal.add(new Transform3D(-0.7, 0, 0));
-    sender.setViaPoint(via_point.toTransform2D().getPositionVector());
-  }
-
-  @Override
-  protected void execute() {
-      if (limeLoc.attemptUpdatePose() && (getDistanceError() > 0.05)) { // TODO: Make this distance tunable
-      computeAndUpdateGoal();
+        NetworkTable tebConfigTable = NetworkTableInstance.getDefault().getTable("TEBPlanner/Config");
+        tebConfigTable.getEntry("TEBReset").setBoolean(true);
+        tebConfigTable.getEntry("MaxVelX").setNumber(1.5);
+        tebConfigTable.getEntry("MaxVelXBackwards").setNumber(1.4);
+        tebConfigTable.getEntry("AccLimX").setNumber(1.0);
+        tebConfigTable.getEntry("MaxVelTheta").setNumber(5.0);
+        tebConfigTable.getEntry("AccLimTheta").setNumber(12.0);
+        if (limeLoc.attemptUpdatePose()) { // TODO: Make this distance tunable
+            computeAndUpdateGoal();
+        } else {
+            if (limeLoc.millisSinceLastAcquired() < 10000) {
+                updateGoal(lastOdomToLimelight);
+            } else {
+                //      Robot.leds.set(ColorPattern.RED);
+                cancel();
+            }
+        }
     }
 
-    // Send velocity command
-    Twist2D cmdVel = receiver.getCmdVel();
-    twist2DInput.setTwist(cmdVel);
-    pipeline.execute();
-  }
-
-  @Override
-  protected boolean isFinished() {
-    if (goal == null) {
-      return true;
+    private void computeAndUpdateGoal() {
+        updateGoal(computeGoal());
     }
-    if (getDistanceError() < 0.02 && Math.abs(getAngleError()) < Math.toRadians(3)) {
-      dt.stop();
+
+    private Transform3D computeGoal() {
+        return driveOdometry.getOdomToBaseLink()
+            .add(limeLoc.getBaseLinkToVisionTarget())
+            .add(new Transform3D(new Vector3D(-0.65, -0.025, 0), Rotation.IDENTITY));
+    }
+
+    private void updateGoal(Transform3D newGoal) {
+        this.goal = newGoal;
+        sender.setGoal(goal.toTransform2D());
+        System.out.println("Goal updated");
+
+        Transform3D via_point = goal.add(new Transform3D(-0.7, 0, 0));
+        sender.setViaPoint(via_point.toTransform2D().getPositionVector());
+    }
+
+    @Override
+    protected void execute() {
+        if (limeLoc.attemptUpdatePose() && (getDistanceError() > 0.05)) { // TODO: Make this distance tunable
+            computeAndUpdateGoal();
+        }
+
+        // Send velocity command
+        Twist2D cmdVel = receiver.getCmdVel();
+        twist2DInput.setTwist(cmdVel);
+        pipeline.execute();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        if (goal == null) {
+            return true;
+        }
+        if (getDistanceError() < 0.02 && Math.abs(getAngleError()) < Math.toRadians(3)) {
+            dt.stop();
 //      Robot.leds.set(ColorPattern.LIME);
-      return true;
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  private double getDistanceError() {
-    Vector3D odomPosition = driveOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
-    return goal.toTransform2D().getPositionVector().distance(new Vector2D(odomPosition.getX(), odomPosition.getY()));
-  }
+    private double getDistanceError() {
+        Vector3D odomPosition = driveOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
+        return goal.toTransform2D().getPositionVector().distance(new Vector2D(odomPosition.getX(), odomPosition.getY()));
+    }
 
-  private double getAngleError() {
-      return TrigUtils.signedAngleDifference(goal.toTransform2D().getTheta(), Math.toRadians(-navx.getYaw()));
-  }
+    private double getAngleError() {
+        return TrigUtils.signedAngleDifference(goal.toTransform2D().getTheta(), Math.toRadians(-navx.getYaw()));
+    }
 }
