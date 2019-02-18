08b97f30f8e7059b8672c8a7c8b3b1e5c4063555
wangl5 <wangl5@users.noreply.github.com>
2019-02-17 19:06:32 -0800

    Rename cargo_intake package to cargo

--- src/main/java/org/team1540/robot2019/commands/cargo/EjectThenDown.java
+++ src/main/java/org/team1540/robot2019/commands/cargo/EjectThenDown.java
@@ -1,4 +1,4 @@
-package org.team1540.robot2019.commands.cargo_intake;
+package org.team1540.robot2019.commands.cargo;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
