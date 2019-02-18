016b35efd5a18e91869fd3662e963e4d9466b6fb
ambercmerrill <ambercmerrill@gmail.com>
2019-02-18 10:56:58 -0800

    Take out unnecessary simplecommands take 2

--- src/main/java/org/team1540/robot2019/commands/cargo/FloorIntake.java
+++ src/main/java/org/team1540/robot2019/commands/cargo/FloorIntake.java
@@ -3,13 +3,14 @@ package org.team1540.robot2019.commands.cargo;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
+import org.team1540.robot2019.commands.hatch.RetractHatch;
 import org.team1540.robot2019.commands.wrist.WristDown;
 import org.team1540.rooster.util.SimpleCommand;
 
 public class FloorIntake extends CommandGroup {
 
     public FloorIntake() {
-        addParallel(new SimpleCommand("Retract Hatch", Robot.hatch::retract, Robot.hatch));
+        addParallel(new RetractHatch());
         addSequential(new MoveElevatorToZero());
         addParallel(new Intake());
         addSequential(new WristDown());
