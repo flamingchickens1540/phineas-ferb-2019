2c9dde7e6babe47037196272b390364c79013cda
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-02-18 00:00:49 -0800

    Rename signedAngleError

--- src/main/java/org/team1540/robot2019/utils/TrigUtils.java
+++ src/main/java/org/team1540/robot2019/utils/TrigUtils.java
@@ -2,8 +2,8 @@ package org.team1540.robot2019.utils;
 
 public class TrigUtils {
 
-    public static double signedAngleDifference(double angle1, double angle2) {
-        double diff = (angle2 - angle1 + Math.PI) % (Math.PI * 2) - Math.PI;
+    public static double signedAngleError(double source, double target) {
+        double diff = (target - source + Math.PI) % (Math.PI * 2) - Math.PI;
         return diff < -Math.PI ? diff + (Math.PI * 2) : diff;
     }
 
