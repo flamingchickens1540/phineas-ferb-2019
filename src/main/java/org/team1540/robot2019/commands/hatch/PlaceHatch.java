6d707988e81f650dc45ccdc71f7014cdf8dca649
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-02-17 16:53:30 -0800

    Replace HatchSlideIn and Out with simple commands

--- src/main/java/org/team1540/robot2019/commands/hatch/PlaceHatch.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/PlaceHatch.java
@@ -2,7 +2,9 @@ package org.team1540.robot2019.commands.hatch;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.Tuning;
+import org.team1540.rooster.util.SimpleCommand;
 
 public class PlaceHatch extends CommandGroup {
 
@@ -11,7 +13,7 @@ public class PlaceHatch extends CommandGroup {
 //    addSequential(new WaitCommand(0.3));
     addSequential(new ReleaseHatch());
     addSequential(new WaitCommand(Tuning.hatchPlaceTime));
-    addSequential(new HatchSlideIn());
+    addSequential(new SimpleCommand("Hatch Slide In", Robot.hatchMech::slideIn, Robot.hatchMech));
   }
 
 }
