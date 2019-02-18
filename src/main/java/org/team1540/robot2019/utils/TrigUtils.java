1df56079f6d23524cdfce9f27a33422877ee5275
wangl5 <wangl5@users.noreply.github.com>
2019-02-17 16:11:04 -0800

    Rename signedAngleDifference

--- src/main/java/org/team1540/robot2019/utils/TrigUtils.java
+++ src/main/java/org/team1540/robot2019/utils/TrigUtils.java
@@ -2,7 +2,7 @@ package org.team1540.robot2019.utils;
 
 public class TrigUtils {
 
-  public static double SignedAngleDifference(double angle1, double angle2) {
+    public static double signedAngleDifference(double angle1, double angle2) {
     double diff = (angle2 - angle1 + Math.PI) % (Math.PI * 2) - Math.PI;
     return diff < -Math.PI ? diff + (Math.PI * 2) : diff;
   }
