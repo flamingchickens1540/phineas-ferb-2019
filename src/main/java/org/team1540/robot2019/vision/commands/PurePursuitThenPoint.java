458c6b76004ef0cd7395abce9637f5dd31c7a0a7
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-02-18 17:01:17 -0800

    Add methods to turn leds off

--- src/main/java/org/team1540/robot2019/vision/commands/PurePursuitThenPoint.java
+++ src/main/java/org/team1540/robot2019/vision/commands/PurePursuitThenPoint.java
@@ -1,17 +1,26 @@
 package org.team1540.robot2019.vision.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.team1540.robot2019.Robot;
+import org.team1540.robot2019.commands.drivetrain.DriveForTimeVelocity;
+import org.team1540.rooster.util.SimpleCommand;
 
 public class PurePursuitThenPoint extends CommandGroup {
 
     public PurePursuitThenPoint() {
+        addSequential(new SimpleCommand("ON", () -> Robot.limelight.setLeds(true)));
+        addSequential(new WaitCommand(0.05));
         addSequential(new PurePursuitLineup(Robot.limelightLocalization, Robot.wheelOdometry));
 
         addSequential(new PointLineupSimple());
-//        addSequential(new PointLineup(Robot.limelightLocalization, Robot.wheelOdometry, Robot.lastOdomToVisionTarget));
 
-//        addSequential(new DriveForTimeVelocity(0.4, 0.5));
+        addSequential(new DriveForTimeVelocity(0.6, 0.5));
+        addSequential(new SimpleCommand("OFF", () -> Robot.limelight.setLeds(false)));
     }
 
+    @Override
+    protected void interrupted() {
+        Robot.limelight.setLeds(false);
+    }
 }
