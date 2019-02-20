92d943a5c6a4afc1e13b61dba609473bd5c882cf
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-02-19 20:34:20 -0800

    Reformat networking classes

--- src/main/java/org/team1540/robot2019/networking/UDPOdometryGoalSender.java
+++ src/main/java/org/team1540/robot2019/networking/UDPOdometryGoalSender.java
@@ -14,66 +14,67 @@ import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import org.team1540.robot2019.datastructures.Odometry;
 import org.team1540.robot2019.datastructures.twod.Transform2D;
 
+// TODO: Use logging class
 public class UDPOdometryGoalSender {
 
-  private DatagramSocket clientSocket;
-  private InetAddress address;
-  private String addressString;
-  private int port;
+    private DatagramSocket clientSocket;
+    private InetAddress address;
+    private String addressString;
+    private int port;
 
-  private Transform2D goal;
-  private Odometry odometry;
-  private Vector2D viaPoint;
+    private Transform2D goal;
+    private Odometry odometry;
+    private Vector2D viaPoint;
 
     public UDPOdometryGoalSender(String address, int port) {
-    addressString = address;
-    this.port = port;
-    attemptConnection();
-  }
-
-  public void attemptConnection() {
-    try {
-      this.address = InetAddress.getByName(addressString);
-      this.clientSocket = new DatagramSocket();
-      System.out.println("[UDP Sender]: Connected to " + addressString);
-    } catch (UnknownHostException | SocketException e) {
-      System.out.println("[UDP Sender]: Couldn't connect to " + addressString);
-      e.printStackTrace();
-        autoReconnect();
+        addressString = address;
+        this.port = port;
+        attemptConnection();
     }
-  }
 
-  public void setGoal(Transform2D goal) {
-    this.goal = goal;
-  }
-
-  public void setViaPoint(Vector2D viaPoint) {
-    this.viaPoint = viaPoint;
-  }
-
-  public void setOdometry(Odometry odometry) {
-    this.odometry = odometry;
-  }
-
-  public void sendIt() throws IOException {
-    if (clientSocket == null || goal == null || viaPoint == null || odometry == null) {
-      return;
+    public void attemptConnection() {
+        try {
+            this.address = InetAddress.getByName(addressString);
+            this.clientSocket = new DatagramSocket();
+            System.out.println("[UDP Sender]: Connected to " + addressString);
+        } catch (UnknownHostException | SocketException e) {
+            System.out.println("[UDP Sender]: Couldn't connect to " + addressString);
+            e.printStackTrace();
+            autoReconnect();
+        }
     }
-    byte[] data = ByteBuffer.allocate(Double.BYTES * 10) // TODO: Send a timestamp/counter
-        .putDouble(odometry.getPose().getPosition().getX())
-        .putDouble(odometry.getPose().getPosition().getY())
-        .putDouble(odometry.getPose().getOrientation().getAngles(RotationOrder.XYZ, RotationConvention.FRAME_TRANSFORM)[2])
-        .putDouble(odometry.getTwist().getX())
-        .putDouble(odometry.getTwist().getOmega())
-        .putDouble(goal.getX())
-        .putDouble(goal.getY())
-        .putDouble(goal.getTheta())
-        .putDouble(viaPoint.getX())
-        .putDouble(viaPoint.getY())
-        .array();
-    DatagramPacket sendPacket = new DatagramPacket(data, data.length, address, port);
-    clientSocket.send(sendPacket);
-  }
+
+    public void setGoal(Transform2D goal) {
+        this.goal = goal;
+    }
+
+    public void setViaPoint(Vector2D viaPoint) {
+        this.viaPoint = viaPoint;
+    }
+
+    public void setOdometry(Odometry odometry) {
+        this.odometry = odometry;
+    }
+
+    public void sendIt() throws IOException {
+        if (clientSocket == null || goal == null || viaPoint == null || odometry == null) {
+            return;
+        }
+        byte[] data = ByteBuffer.allocate(Double.BYTES * 10) // TODO: Send a timestamp/counter
+            .putDouble(odometry.getPose().getPosition().getX())
+            .putDouble(odometry.getPose().getPosition().getY())
+            .putDouble(odometry.getPose().getOrientation().getAngles(RotationOrder.XYZ, RotationConvention.FRAME_TRANSFORM)[2])
+            .putDouble(odometry.getTwist().getX())
+            .putDouble(odometry.getTwist().getOmega())
+            .putDouble(goal.getX())
+            .putDouble(goal.getY())
+            .putDouble(goal.getTheta())
+            .putDouble(viaPoint.getX())
+            .putDouble(viaPoint.getY())
+            .array();
+        DatagramPacket sendPacket = new DatagramPacket(data, data.length, address, port);
+        clientSocket.send(sendPacket);
+    }
 
     private void autoReconnect() {
         new Notifier(this::attemptConnection).startSingle(1);
