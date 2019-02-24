239d973162c8edc6baf7f5a8198c79bb45623606
Zachary Robinson <robinsonz@catlin.edu>
2019-02-23 21:08:43 -0800

    Rename Intake to IntakeCargo

--- src/main/java/org/team1540/robot2019/commands/cargo/FloorIntake.java
+++ src/main/java/org/team1540/robot2019/commands/cargo/FloorIntake.java
@@ -1,18 +1,16 @@
 package org.team1540.robot2019.commands.cargo;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
 import org.team1540.robot2019.commands.hatch.RetractHatch;
 import org.team1540.robot2019.commands.wrist.WristDown;
-import org.team1540.rooster.util.SimpleCommand;
 
 public class FloorIntake extends CommandGroup {
 
     public FloorIntake() {
         addParallel(new RetractHatch());
         addSequential(new MoveElevatorToZero());
-        addParallel(new Intake());
+        addParallel(new IntakeCargo());
         addSequential(new WristDown());
     }
 }
