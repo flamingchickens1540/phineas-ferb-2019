e42f736838a87257d68d31f816a63320bb2220dc
Zachary Robinson <robinsonz@catlin.edu>
2019-02-23 21:31:33 -0800

    Make waiting forever more elegant

--- src/main/java/org/team1540/robot2019/commands/hatch/PrepHatchFloorGrab.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/PrepHatchFloorGrab.java
@@ -1,11 +1,11 @@
 package org.team1540.robot2019.commands.hatch;
 
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.team1540.robot2019.Tuning;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
 import org.team1540.robot2019.commands.wrist.WristDown;
+import org.team1540.rooster.util.SimpleLoopCommand;
 
 public class PrepHatchFloorGrab extends CommandGroup {
 
@@ -15,11 +15,6 @@ public class PrepHatchFloorGrab extends CommandGroup {
         addSequential(new WristDown());
         addSequential(new WaitCommand(Tuning.hatchPrepFloorWaitTime));
         addSequential(new ExtendHatchMech());
-        addSequential(new Command() {
-            @Override
-            protected boolean isFinished() {
-                return false;
-            }
-        });
+        addSequential(new SimpleLoopCommand("Wait until interrupted", () -> {}));
     }
 }
