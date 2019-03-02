b1191a760df9ca786c9ce94c643fa324e9825172
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-02 11:42:27 -0800

    Rename manual drive lineup

--- src/main/java/org/team1540/robot2019/commands/auto/PurePursuitThenPointToVisionTarget.java
+++ src/main/java/org/team1540/robot2019/commands/auto/PurePursuitThenPointToVisionTarget.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.robot2019.Robot;
-import org.team1540.robot2019.commands.drivetrain.TankDriveForTimeVelocity;
 
 public class PurePursuitThenPointToVisionTarget extends CommandGroup {
 
@@ -12,7 +11,7 @@ public class PurePursuitThenPointToVisionTarget extends CommandGroup {
         addSequential(new WaitCommand(0.05)); // Wait for leds to turn on
 //        addSequential(new PurePursuitToVisionTarget(Robot.deepSpaceVisionTargetLocalization, Robot.odometry, this::cancel));
 
-        addSequential(new SimplePointToVisionTargetAndDrive());
+        addSequential(new SimplePointToVisionTargetAndManualDrive());
 //        addSequential(new SimplePointToVisionTarget());
 
 //        addSequential(new TankDriveForTimeVelocity(0.6, 0.5)); // TODO: Straight driving with navx
