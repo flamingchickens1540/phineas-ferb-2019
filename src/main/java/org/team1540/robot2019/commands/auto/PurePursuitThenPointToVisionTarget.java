5ed0f05e79119c7af088a9d39f0321b4cbdd08a3
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-01 21:04:16 -0800

    Adjust tuning and replace auto-lineup with manual point

--- src/main/java/org/team1540/robot2019/commands/auto/PurePursuitThenPointToVisionTarget.java
+++ src/main/java/org/team1540/robot2019/commands/auto/PurePursuitThenPointToVisionTarget.java
@@ -2,6 +2,7 @@ package org.team1540.robot2019.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.commands.drivetrain.TankDriveForTimeVelocity;
 
@@ -9,16 +10,19 @@ public class PurePursuitThenPointToVisionTarget extends CommandGroup {
 
     public PurePursuitThenPointToVisionTarget() {
         addSequential(new WaitCommand(0.05)); // Wait for leds to turn on
-        addSequential(new PurePursuitToVisionTarget(Robot.deepSpaceVisionTargetLocalization, Robot.odometry, this::cancel));
+//        addSequential(new PurePursuitToVisionTarget(Robot.deepSpaceVisionTargetLocalization, Robot.odometry, this::cancel));
 
-        addSequential(new SimplePointToVisionTarget());
+        addSequential(new SimplePointToVisionTargetAndDrive());
+//        addSequential(new SimplePointToVisionTarget());
 
-        addSequential(new TankDriveForTimeVelocity(0.6, 0.5)); // TODO: Straight driving with navx
+//        addSequential(new TankDriveForTimeVelocity(0.6, 0.5)); // TODO: Straight driving with navx
     }
 
     @Override
     protected void initialize() {
-        Robot.limelight.setLeds(true);
+        if (SmartDashboard.getBoolean("TurnOffLimelightWhenNotInUse", true)) {
+            Robot.limelight.setLeds(true);
+        }
     }
 
     @Override
@@ -28,6 +32,7 @@ public class PurePursuitThenPointToVisionTarget extends CommandGroup {
 
     @Override
     protected void end() {
-        Robot.limelight.setLeds(false);
-    }
+        if (SmartDashboard.getBoolean("TurnOffLimelightWhenNotInUse", true)) {
+            Robot.limelight.setLeds(false);
+        }    }
 }
