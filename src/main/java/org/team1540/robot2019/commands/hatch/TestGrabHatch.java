90e18f0583a7579a6a11754c48cfc68a16da80cd
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-05 17:17:49 -0800

    Remove drive forward from grab hatch

--- src/main/java/org/team1540/robot2019/commands/hatch/TestGrabHatch.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/TestGrabHatch.java
@@ -1,18 +1,15 @@
 package org.team1540.robot2019.commands.hatch;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.TimedCommand;
 import edu.wpi.first.wpilibj.command.WaitCommand;
-import org.team1540.robot2019.commands.drivetrain.TankDriveForTimePercent;
-import org.team1540.rooster.util.SimpleCommand;
 
 public class TestGrabHatch extends CommandGroup {
 
     public TestGrabHatch() {
         addSequential(new ReleaseHatch());
         addSequential(new ExtendHatchMech());
-        addSequential(new SimpleCommand("Drive", () -> new TankDriveForTimePercent(0.2, 0.3).start()));
-        addSequential(new TimedCommand(0.2));
+//        addSequential(new SimpleCommand("Drive", () -> new TankDriveForTimePercent(0.2, 0.3).start()));
+//        addSequential(new TimedCommand(0.2));
         addSequential(new WaitCommand(0.2));
         addSequential(new GrabHatch());
         addSequential(new WaitCommand(0.2));
