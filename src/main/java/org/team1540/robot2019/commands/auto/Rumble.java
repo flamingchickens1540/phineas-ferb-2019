9e4c031a3b3a006d4f4a22a7d27f33479df729ad
wangl5 <wangl5@users.noreply.github.com>
2019-03-05 16:28:24 -0800

    Add rumble commands

---
+++ src/main/java/org/team1540/robot2019/commands/auto/Rumble.java
@@ -0,0 +1,28 @@
+package org.team1540.robot2019.commands.auto;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Rumble extends Command {
+
+    private GenericHID xboxController;
+    private double value;
+
+    public Rumble(XboxController xboxController, double value) {
+        this.xboxController = xboxController;
+        this.value = value;
+    }
+
+    @Override
+    protected void initialize() {
+        xboxController.setRumble(RumbleType.kLeftRumble, value);
+        xboxController.setRumble(RumbleType.kRightRumble, value);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+}
