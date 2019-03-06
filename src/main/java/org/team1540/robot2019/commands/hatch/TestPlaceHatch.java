6c84ed45866a4c46679cd57d50874cb5e0a98f9d
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-05 16:27:11 -0800

    Fix drivetrain requirement for grabbing and placing hatches

--- src/main/java/org/team1540/robot2019/commands/hatch/TestPlaceHatch.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/TestPlaceHatch.java
@@ -1,16 +1,19 @@
 package org.team1540.robot2019.commands.hatch;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.team1540.robot2019.commands.drivetrain.TankDriveForTimePercent;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
+import org.team1540.rooster.util.SimpleCommand;
 
 public class TestPlaceHatch extends CommandGroup {
 
     public TestPlaceHatch() {
         addSequential(new GrabHatch());
         addSequential(new ExtendHatchMech());
-        addSequential(new TankDriveForTimePercent(0.3, 0.3));
+        addSequential(new SimpleCommand("Drive", () -> new TankDriveForTimePercent(0.3, 0.3).start()));
+        addSequential(new TimedCommand(0.3));
         addSequential(new WaitCommand(0.2));
         addSequential(new ReleaseHatch());
         addSequential(new WaitCommand(0.1));
