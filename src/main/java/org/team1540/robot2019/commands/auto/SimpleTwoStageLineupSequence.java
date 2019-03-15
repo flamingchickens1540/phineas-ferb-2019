a467489f1d5c3c333067175e1cc9f4b742140886
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-14 20:49:28 -0700

    Comment out unused things that are too good to delete

--- src/main/java/org/team1540/robot2019/commands/auto/SimpleTwoStageLineupSequence.java
+++ src/main/java/org/team1540/robot2019/commands/auto/SimpleTwoStageLineupSequence.java
@@ -1,36 +1,36 @@
-package org.team1540.robot2019.commands.auto;
-
-import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.team1540.robot2019.Robot;
-import org.team1540.robot2019.commands.drivetrain.TankDriveForTimeVelocity;
-
-public class SimpleTwoStageLineupSequence extends CommandGroup {
-
-    public SimpleTwoStageLineupSequence() {
-        addSequential(new WaitCommand(0.05)); // Wait for leds to turn on
-        addSequential(new PurePursuitToVisionTarget(Robot.deepSpaceVisionTargetLocalization, Robot.odometry, this::cancel)); // TODO: Replace this with getGroup().cancel()
-        addSequential(new SimplePointToVisionTarget());
-        addSequential(new TankDriveForTimeVelocity(0.6, 0.5)); // TODO: Straight driving with navx
-    }
-
-    @Override
-    protected void initialize() {
-        if (SmartDashboard.getBoolean("TurnOffLimelightWhenNotInUse", true)) {
-            Robot.limelight.prepForVision();
-        }
-    }
-
-    @Override
-    protected void interrupted() {
-        end(); // CommandGroups don't call end by default on interrupted
-    }
-
-    @Override
-    protected void end() {
-        if (SmartDashboard.getBoolean("TurnOffLimelightWhenNotInUse", true)) {
-            Robot.limelight.prepForDriverCam();
-        }
-    }
-}
+//package org.team1540.robot2019.commands.auto;
+//
+//import edu.wpi.first.wpilibj.command.CommandGroup;
+//import edu.wpi.first.wpilibj.command.WaitCommand;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import org.team1540.robot2019.Robot;
+//import org.team1540.robot2019.commands.drivetrain.TankDriveForTimeVelocity;
+//
+//public class SimpleTwoStageLineupSequence extends CommandGroup {
+//
+//    public SimpleTwoStageLineupSequence() {
+//        addSequential(new WaitCommand(0.05)); // Wait for leds to turn on
+//        addSequential(new PurePursuitToVisionTarget(Robot.deepSpaceVisionTargetLocalization, Robot.odometry, this::cancel)); // TODO: Replace this with getGroup().cancel()
+////        addSequential(new SimplePointToVisionTarget());
+//        addSequential(new TankDriveForTimeVelocity(0.6, 0.5)); // TODO: Straight driving with navx
+//    }
+//
+//    @Override
+//    protected void initialize() {
+//        if (SmartDashboard.getBoolean("TurnOffLimelightWhenNotInUse", true)) {
+//            Robot.limelight.prepForVision();
+//        }
+//    }
+//
+//    @Override
+//    protected void interrupted() {
+//        end(); // CommandGroups don't call end by default on interrupted
+//    }
+//
+//    @Override
+//    protected void end() {
+//        if (SmartDashboard.getBoolean("TurnOffLimelightWhenNotInUse", true)) {
+//            Robot.limelight.prepForDriverCam();
+//        }
+//    }
+//}
