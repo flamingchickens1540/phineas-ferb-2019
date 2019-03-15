a0e1574d78ce35741fa818127635a376599ce055
wangl5 <wangl5@users.noreply.github.com>
2019-03-14 20:49:28 -0700

    Comment out unused things that are too good to delete

--- src/main/java/org/team1540/robot2019/commands/auto/SimplePointToVisionTarget.java
+++ src/main/java/org/team1540/robot2019/commands/auto/SimplePointToVisionTarget.java
@@ -1,121 +1,121 @@
-package org.team1540.robot2019.commands.auto;
-
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.command.PIDCommand;
-import org.apache.log4j.Logger;
-import org.team1540.robot2019.Hardware;
-import org.team1540.robot2019.Robot;
-import org.team1540.robot2019.Tuning;
-import org.team1540.robot2019.datastructures.threed.Transform3D;
-import org.team1540.robot2019.datastructures.twod.Twist2D;
-import org.team1540.robot2019.datastructures.utils.TrigUtils;
-import org.team1540.robot2019.utils.ControlUtils;
-import org.team1540.robot2019.utils.TankDriveTwist2DInput;
-import org.team1540.rooster.drive.pipeline.FeedForwardProcessor;
-import org.team1540.rooster.drive.pipeline.UnitScaler;
-import org.team1540.rooster.functional.Executable;
-
-public class SimplePointToVisionTarget extends PIDCommand {
-
-    public static final Logger logger = Logger.getLogger(SimplePointToVisionTarget.class);
-
-    // Max/Min angular velocity
-    private static final double MIN_VEL_THETA = 0.2;
-    private static final double MAX_VEL_THETA = 1.8;
-
-    // Constants for angular VPID controller
-    private static final double ANGULAR_KP = -6;
-    private static final double ANGULAR_KI = 0;
-    private static final double ANGULAR_KD = -10;
-
-    private static final double ANGLE_OFFSET = Math.toRadians(5.5); // Degrees offset from center of target
-
-    // Goal tolerances for angle
-    private static final double GOAL_TOLERANCE_ANGULAR_POSITION = Math.toRadians(0.6);
-    private static final double GOAL_TOLERANCE_ANGULAR_VELOCITY = 0.3;
-
-    private Executable pipeline;
-    private TankDriveTwist2DInput twist2DInput;
-    private Double goal = null;
-
-    public SimplePointToVisionTarget() { // TODO: Separate vision processing and point to angle
-        super(ANGULAR_KP, ANGULAR_KI, ANGULAR_KD);
-        requires(Robot.drivetrain);
-        twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadiusMeters);
-        pipeline = twist2DInput
-            .then(new FeedForwardProcessor(0, 0, 0))
-            .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
-            .then(Robot.drivetrain.getPipelineOutput());
-    }
-
-    @Override
-    protected void initialize() {
-        double x = Robot.limelight.getTargetAngles().getX();
-        if (x == 0) {
-            Transform3D prevGoal = Robot.lastOdomToVisionTargetTracker.getOdomToVisionTarget();
-            if (prevGoal == null) {
-                Robot.drivetrain.stop();
-                logger.warn("Unable to find target and no alternative specified! Ending...");
-                return;
-            }
-            goal = prevGoal.toTransform2D().getTheta();
-            logger.debug("Unable to find target. Using alternative goal angle: " + goal);
-        } else {
-            goal = -(x - ANGLE_OFFSET) + Hardware.navx.getYawRadians(); // TODO: This should not be duplicated twice
-            logger.debug("Point lineup simple starting. Initial goal angle: " + goal);
-        }
-    }
-
-    @Override
-    protected void execute() {
-        if (goal == null) {
-            return;
-        }
-        double x = Math.toRadians(NetworkTableInstance.getDefault().getTable("limelight-a").getEntry("tx").getDouble(0)); // TODO: Are you going to use the interface or not?
-        if (x != 0) {
-            goal = -(x - ANGLE_OFFSET) + Hardware.navx.getYawRadians();
-        }
-    }
-
-    @Override
-    protected boolean isFinished() {
-        if (goal == null) {
-            return true;
-        }
-        double anglePosError = Math.abs(getAngleError(goal));
-        double angleVelError = Math.abs(Robot.drivetrain.getTwist().getOmega());
-        boolean isFinished = anglePosError < GOAL_TOLERANCE_ANGULAR_POSITION && angleVelError < GOAL_TOLERANCE_ANGULAR_VELOCITY;
-        if (isFinished) {
-            logger.debug(String.format("Simple point goal reached! Angle error remaining: %f Angular velocity error remaining: %f", anglePosError, angleVelError));
-        }
-        return isFinished;
-    }
-
-    @Override
-    protected void end() {
-        logger.debug("SimplePointToTarget Ended!");
-        Robot.drivetrain.stop();
-    }
-
-    private double getAngleError(double x) {
-        return TrigUtils.signedAngleError(x, Hardware.navx.getYawRadians());
-    }
-
-    @Override
-    protected double returnPIDInput() {
-        if (goal == null) {
-            return 0;
-        }
-        return getAngleError(goal);
-    }
-
-    @Override
-    protected void usePIDOutput(double output) {
-        output *= MAX_VEL_THETA;
-        double cmdVelTheta = ControlUtils.velocityPosNegConstrain(output, MAX_VEL_THETA, MIN_VEL_THETA);
-
-        Twist2D cmdVel = new Twist2D(0, 0, cmdVelTheta);
-        twist2DInput.setTwist(cmdVel);
-        pipeline.execute();
-    }
-}
+//package org.team1540.robot2019.commands.auto;
+//
+//import edu.wpi.first.networktables.NetworkTableInstance;
+//import edu.wpi.first.wpilibj.command.PIDCommand;
+//import org.apache.log4j.Logger;
+//import org.team1540.robot2019.Hardware;
+//import org.team1540.robot2019.Robot;
+//import org.team1540.robot2019.Tuning;
+//import org.team1540.robot2019.datastructures.threed.Transform3D;
+//import org.team1540.robot2019.datastructures.twod.Twist2D;
+//import org.team1540.robot2019.datastructures.utils.TrigUtils;
+//import org.team1540.robot2019.utils.ControlUtils;
+//import org.team1540.robot2019.utils.TankDriveTwist2DInput;
+//import org.team1540.rooster.drive.pipeline.FeedForwardProcessor;
+//import org.team1540.rooster.drive.pipeline.UnitScaler;
+//import org.team1540.rooster.functional.Executable;
+//
+//public class SimplePointToVisionTarget extends PIDCommand {
+//
+//    public static final Logger logger = Logger.getLogger(SimplePointToVisionTarget.class);
+//
+//    // Max/Min angular velocity
+//    private static final double MIN_VEL_THETA = 0.2;
+//    private static final double MAX_VEL_THETA = 1.8;
+//
+//    // Constants for angular VPID controller
+//    private static final double ANGULAR_KP = -6;
+//    private static final double ANGULAR_KI = 0;
+//    private static final double ANGULAR_KD = -10;
+//
+//    private static final double ANGLE_OFFSET = Math.toRadians(5.5); // Degrees offset from center of target
+//
+//    // Goal tolerances for angle
+//    private static final double GOAL_TOLERANCE_ANGULAR_POSITION = Math.toRadians(0.6);
+//    private static final double GOAL_TOLERANCE_ANGULAR_VELOCITY = 0.3;
+//
+//    private Executable pipeline;
+//    private TankDriveTwist2DInput twist2DInput;
+//    private Double goal = null;
+//
+//    public SimplePointToVisionTarget() { // TODO: Separate vision processing and point to angle
+//        super(ANGULAR_KP, ANGULAR_KI, ANGULAR_KD);
+//        requires(Robot.drivetrain);
+//        twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadiusMeters);
+//        pipeline = twist2DInput
+//            .then(new FeedForwardProcessor(0, 0, 0))
+//            .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
+//            .then(Robot.drivetrain.getPipelineOutput());
+//    }
+//
+//    @Override
+//    protected void initialize() {
+//        double x = Robot.limelight.getTargetAngles().getX();
+//        if (x == 0) {
+//            Transform3D prevGoal = Robot.lastOdomToVisionTargetTracker.getOdomToVisionTarget();
+//            if (prevGoal == null) {
+//                Robot.drivetrain.stop();
+//                logger.warn("Unable to find target and no alternative specified! Ending...");
+//                return;
+//            }
+//            goal = prevGoal.toTransform2D().getTheta();
+//            logger.debug("Unable to find target. Using alternative goal angle: " + goal);
+//        } else {
+//            goal = -(x - ANGLE_OFFSET) + Hardware.navx.getYawRadians(); // TODO: This should not be duplicated twice
+//            logger.debug("Point lineup simple starting. Initial goal angle: " + goal);
+//        }
+//    }
+//
+//    @Override
+//    protected void execute() {
+//        if (goal == null) {
+//            return;
+//        }
+//        double x = Math.toRadians(NetworkTableInstance.getDefault().getTable("limelight-a").getEntry("tx").getDouble(0)); // TODO: Are you going to use the interface or not?
+//        if (x != 0) {
+//            goal = -(x - ANGLE_OFFSET) + Hardware.navx.getYawRadians();
+//        }
+//    }
+//
+//    @Override
+//    protected boolean isFinished() {
+//        if (goal == null) {
+//            return true;
+//        }
+//        double anglePosError = Math.abs(getAngleError(goal));
+//        double angleVelError = Math.abs(Robot.drivetrain.getTwist().getOmega());
+//        boolean isFinished = anglePosError < GOAL_TOLERANCE_ANGULAR_POSITION && angleVelError < GOAL_TOLERANCE_ANGULAR_VELOCITY;
+//        if (isFinished) {
+//            logger.debug(String.format("Simple point goal reached! Angle error remaining: %f Angular velocity error remaining: %f", anglePosError, angleVelError));
+//        }
+//        return isFinished;
+//    }
+//
+//    @Override
+//    protected void end() {
+//        logger.debug("SimplePointToTarget Ended!");
+//        Robot.drivetrain.stop();
+//    }
+//
+//    private double getAngleError(double x) {
+//        return TrigUtils.signedAngleError(x, Hardware.navx.getYawRadians());
+//    }
+//
+//    @Override
+//    protected double returnPIDInput() {
+//        if (goal == null) {
+//            return 0;
+//        }
+//        return getAngleError(goal);
+//    }
+//
+//    @Override
+//    protected void usePIDOutput(double output) {
+//        output *= MAX_VEL_THETA;
+//        double cmdVelTheta = ControlUtils.velocityPosNegConstrain(output, MAX_VEL_THETA, MIN_VEL_THETA);
+//
+//        Twist2D cmdVel = new Twist2D(0, 0, cmdVelTheta);
+//        twist2DInput.setTwist(cmdVel);
+//        pipeline.execute();
+//    }
+//}
--- src/main/java/org/team1540/robot2019/commands/auto/UDPAutoLineup.java
+++ src/main/java/org/team1540/robot2019/commands/auto/UDPAutoLineup.java
@@ -1,127 +1,127 @@
-package org.team1540.robot2019.commands.auto;
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.command.Command;
-import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
-import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
-import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
-import org.team1540.robot2019.Tuning;
-import org.team1540.robot2019.datastructures.threed.Transform3D;
-import org.team1540.robot2019.datastructures.twod.Twist2D;
-import org.team1540.robot2019.datastructures.utils.TrigUtils;
-import org.team1540.robot2019.odometry.tankdrive.TankDriveOdometryRunnable;
-import org.team1540.robot2019.subsystems.Drivetrain;
-import org.team1540.robot2019.utils.TankDriveTwist2DInput;
-import org.team1540.robot2019.vision.deepspace.DeepSpaceVisionTargetLocalization;
-import org.team1540.robot2019.wrappers.NavX;
-import org.team1540.robot2019.wrappers.TEBPlanner;
-import org.team1540.rooster.drive.pipeline.FeedForwardProcessor;
-import org.team1540.rooster.drive.pipeline.UnitScaler;
-import org.team1540.rooster.functional.Executable;
-
-public class UDPAutoLineup extends Command {
-
-    private final Drivetrain dt;
-    private final TEBPlanner planner;
-    private final DeepSpaceVisionTargetLocalization limeLoc;
-    private final TankDriveOdometryRunnable driveOdometry;
-    private final Transform3D lastOdomToLimelight;
-    private final NavX navx;
-
-    private Transform3D goal;
-    private Executable pipeline;
-    private TankDriveTwist2DInput twist2DInput;
-
-    public UDPAutoLineup(Drivetrain dt, TEBPlanner planner, DeepSpaceVisionTargetLocalization limeLoc, TankDriveOdometryRunnable driveOdometry,
-        Transform3D lastOdomToLimelight, NavX navx) {
-        this.dt = dt;
-        this.planner = planner;
-        this.limeLoc = limeLoc;
-        this.driveOdometry = driveOdometry;
-        this.lastOdomToLimelight = lastOdomToLimelight;
-        this.navx = navx;
-        requires(dt);
-        twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadiusMeters);
-        pipeline = twist2DInput
-            .then(new FeedForwardProcessor(0, 0, 0))
-//        .then(new FeedForwardProcessor(0.27667, 0.054083,0.08694))
-            // .then((Processor<TankDriveData, TankDriveData>) tankDriveData -> new TankDriveData(tankDriveData.left, tankDriveData.right))
-            .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
-            .then(dt.getPipelineOutput());
-    }
-
-    @Override
-    protected void initialize() {
-        NetworkTable tebConfigTable = NetworkTableInstance.getDefault().getTable("TEBPlanner/Config");
-        tebConfigTable.getEntry("TEBReset").setBoolean(true);
-        tebConfigTable.getEntry("MaxVelX").setNumber(1.5);
-        tebConfigTable.getEntry("MaxVelXBackwards").setNumber(1.4);
-        tebConfigTable.getEntry("AccLimX").setNumber(1.0);
-        tebConfigTable.getEntry("MaxVelTheta").setNumber(5.0);
-        tebConfigTable.getEntry("AccLimTheta").setNumber(12.0);
-        if (limeLoc.attemptUpdatePose()) { // TODO: Make this distance tunable
-            computeAndUpdateGoal();
-        } else {
-            if (limeLoc.millisSinceLastAcquired() < 10000) {
-                updateGoal(lastOdomToLimelight);
-            } else {
-                //      Robot.leds.set(ColorPattern.RED);
-                cancel();
-            }
-        }
-    }
-
-    private void computeAndUpdateGoal() {
-        updateGoal(computeGoal());
-    }
-
-    private Transform3D computeGoal() {
-        return driveOdometry.getOdomToBaseLink()
-            .add(limeLoc.getLastBaseLinkToVisionTarget())
-            .add(new Transform3D(new Vector3D(-0.65, -0.025, 0), Rotation.IDENTITY));
-    }
-
-    private void updateGoal(Transform3D newGoal) {
-        this.goal = newGoal;
-        planner.setGoal(goal.toTransform2D());
-        System.out.println("Goal updated");
-
-        Transform3D via_point = goal.add(new Transform3D(-0.7, 0, 0));
-        planner.setViaPoint(via_point.toTransform2D().getPositionVector());
-    }
-
-    @Override
-    protected void execute() {
-        if (limeLoc.attemptUpdatePose() && (getDistanceError() > 0.05)) { // TODO: Make this distance tunable
-            computeAndUpdateGoal();
-        }
-
-        // Send velocity command
-        Twist2D cmdVel = planner.getCmdVel();
-        twist2DInput.setTwist(cmdVel);
-        pipeline.execute();
-    }
-
-    @Override
-    protected boolean isFinished() {
-        if (goal == null) {
-            return true;
-        }
-        if (getDistanceError() < 0.02 && Math.abs(getAngleError()) < Math.toRadians(3)) {
-            dt.stop();
-//      Robot.leds.set(ColorPattern.LIME);
-            return true;
-        }
-        return false;
-    }
-
-    private double getDistanceError() {
-        Vector3D odomPosition = driveOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
-        return goal.toTransform2D().getPositionVector().distance(new Vector2D(odomPosition.getX(), odomPosition.getY()));
-    }
-
-    private double getAngleError() {
-        return TrigUtils.signedAngleError(navx.getYawRadians(), goal.toTransform2D().getTheta());
-    }
-}
+//package org.team1540.robot2019.commands.auto;
+//
+//import edu.wpi.first.networktables.NetworkTable;
+//import edu.wpi.first.networktables.NetworkTableInstance;
+//import edu.wpi.first.wpilibj.command.Command;
+//import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
+//import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+//import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+//import org.team1540.robot2019.Tuning;
+//import org.team1540.robot2019.datastructures.threed.Transform3D;
+//import org.team1540.robot2019.datastructures.twod.Twist2D;
+//import org.team1540.robot2019.datastructures.utils.TrigUtils;
+//import org.team1540.robot2019.odometry.tankdrive.TankDriveOdometryRunnable;
+//import org.team1540.robot2019.subsystems.Drivetrain;
+//import org.team1540.robot2019.utils.TankDriveTwist2DInput;
+//import org.team1540.robot2019.vision.deepspace.DeepSpaceVisionTargetLocalization;
+//import org.team1540.robot2019.wrappers.NavX;
+//import org.team1540.robot2019.wrappers.TEBPlanner;
+//import org.team1540.rooster.drive.pipeline.FeedForwardProcessor;
+//import org.team1540.rooster.drive.pipeline.UnitScaler;
+//import org.team1540.rooster.functional.Executable;
+//
+//public class UDPAutoLineup extends Command {
+//
+//    private final Drivetrain dt;
+//    private final TEBPlanner planner;
+//    private final DeepSpaceVisionTargetLocalization limeLoc;
+//    private final TankDriveOdometryRunnable driveOdometry;
+//    private final Transform3D lastOdomToLimelight;
+//    private final NavX navx;
+//
+//    private Transform3D goal;
+//    private Executable pipeline;
+//    private TankDriveTwist2DInput twist2DInput;
+//
+//    public UDPAutoLineup(Drivetrain dt, TEBPlanner planner, DeepSpaceVisionTargetLocalization limeLoc, TankDriveOdometryRunnable driveOdometry,
+//        Transform3D lastOdomToLimelight, NavX navx) {
+//        this.dt = dt;
+//        this.planner = planner;
+//        this.limeLoc = limeLoc;
+//        this.driveOdometry = driveOdometry;
+//        this.lastOdomToLimelight = lastOdomToLimelight;
+//        this.navx = navx;
+//        requires(dt);
+//        twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadiusMeters);
+//        pipeline = twist2DInput
+//            .then(new FeedForwardProcessor(0, 0, 0))
+////        .then(new FeedForwardProcessor(0.27667, 0.054083,0.08694))
+//            // .then((Processor<TankDriveData, TankDriveData>) tankDriveData -> new TankDriveData(tankDriveData.left, tankDriveData.right))
+//            .then(new UnitScaler(Tuning.drivetrainTicksPerMeter, 10))
+//            .then(dt.getPipelineOutput());
+//    }
+//
+//    @Override
+//    protected void initialize() {
+//        NetworkTable tebConfigTable = NetworkTableInstance.getDefault().getTable("TEBPlanner/Config");
+//        tebConfigTable.getEntry("TEBReset").setBoolean(true);
+//        tebConfigTable.getEntry("MaxVelX").setNumber(1.5);
+//        tebConfigTable.getEntry("MaxVelXBackwards").setNumber(1.4);
+//        tebConfigTable.getEntry("AccLimX").setNumber(1.0);
+//        tebConfigTable.getEntry("MaxVelTheta").setNumber(5.0);
+//        tebConfigTable.getEntry("AccLimTheta").setNumber(12.0);
+//        if (limeLoc.attemptUpdatePose()) { // TODO: Make this distance tunable
+//            computeAndUpdateGoal();
+//        } else {
+//            if (limeLoc.millisSinceLastAcquired() < 10000) {
+//                updateGoal(lastOdomToLimelight);
+//            } else {
+//                //      Robot.leds.set(ColorPattern.RED);
+//                cancel();
+//            }
+//        }
+//    }
+//
+//    private void computeAndUpdateGoal() {
+//        updateGoal(computeGoal());
+//    }
+//
+//    private Transform3D computeGoal() {
+//        return driveOdometry.getOdomToBaseLink()
+//            .add(limeLoc.getLastBaseLinkToVisionTarget())
+//            .add(new Transform3D(new Vector3D(-0.65, -0.025, 0), Rotation.IDENTITY));
+//    }
+//
+//    private void updateGoal(Transform3D newGoal) {
+//        this.goal = newGoal;
+//        planner.setGoal(goal.toTransform2D());
+//        System.out.println("Goal updated");
+//
+//        Transform3D via_point = goal.add(new Transform3D(-0.7, 0, 0));
+//        planner.setViaPoint(via_point.toTransform2D().getPositionVector());
+//    }
+//
+//    @Override
+//    protected void execute() {
+//        if (limeLoc.attemptUpdatePose() && (getDistanceError() > 0.05)) { // TODO: Make this distance tunable
+//            computeAndUpdateGoal();
+//        }
+//
+//        // Send velocity command
+//        Twist2D cmdVel = planner.getCmdVel();
+//        twist2DInput.setTwist(cmdVel);
+//        pipeline.execute();
+//    }
+//
+//    @Override
+//    protected boolean isFinished() {
+//        if (goal == null) {
+//            return true;
+//        }
+//        if (getDistanceError() < 0.02 && Math.abs(getAngleError()) < Math.toRadians(3)) {
+//            dt.stop();
+////      Robot.leds.set(ColorPattern.LIME);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    private double getDistanceError() {
+//        Vector3D odomPosition = driveOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
+//        return goal.toTransform2D().getPositionVector().distance(new Vector2D(odomPosition.getX(), odomPosition.getY()));
+//    }
+//
+//    private double getAngleError() {
+//        return TrigUtils.signedAngleError(navx.getYawRadians(), goal.toTransform2D().getTheta());
+//    }
+//}
