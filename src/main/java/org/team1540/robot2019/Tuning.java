748add3cc0e8e480a043ffbc24f6e4cc4cd142d7
Zachary Robinson <robinsonz@catlin.edu>
2019-03-15 18:33:36 -0700

    Increase wrist up throttle

--- src/main/java/org/team1540/robot2019/Tuning.java
+++ src/main/java/org/team1540/robot2019/Tuning.java
@@ -85,7 +85,7 @@ public class Tuning {
     public static double wristDownTravelPwrThrot = 0.7;
     public static double wristDownTravelBrakeThrot = 0.0;
     // what's uptravel
-    public static double wristUpTravelThrot = 0.55;
+    public static double wristUpTravelThrot = .6;
     public static double wristHoldThrot = 0.1;
     public static double wristLowerTimeout = 1;
 
