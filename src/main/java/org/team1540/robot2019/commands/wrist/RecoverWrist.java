6c4120f83b4fc8fafeeb23b061edac6498a3ffff
Zachary Robinson <robinsonz@catlin.edu>
2019-03-15 18:14:21 -0700

    Add wrist recover command

---
+++ src/main/java/org/team1540/robot2019/commands/wrist/RecoverWrist.java
@@ -0,0 +1,33 @@
+package org.team1540.robot2019.commands.wrist;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.team1540.robot2019.Robot;
+
+public class RecoverWrist extends Command {
+
+    public RecoverWrist() {
+        requires(Robot.wrist);
+    }
+
+    @Override
+    protected void initialize() {
+        Robot.wrist.set(1);
+
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected void end() {
+        Robot.wrist.set(0);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+
+}
