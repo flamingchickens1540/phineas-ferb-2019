5a02101d944a839165e7d25d1bc63179e16b6f50
wangl <{ID}+{username}@users.noreply.github.com`>
2019-03-18 13:19:45 -0700

    Refactor percent manual lineup localization

--- src/main/java/org/team1540/robot2019/commands/auto/PercentManualLineupLocalization.java
+++ src/main/java/org/team1540/robot2019/commands/auto/PercentManualLineupLocalization.java
@@ -25,30 +25,21 @@ public class PercentManualLineupLocalization extends PIDCommand {
 
     private static double OUTPUT_SCALAR = 20;
 
-    private static double DEADZONE_VEL_THETA = 0.05;
+    private static double DEADZONE = 0.05;
 
     // Max/Min angular velocity
-    private static double MIN_VEL_THETA = 0;
-    private static double MAX_VEL_THETA = 10;
+    private static double MIN = 0;
+    private static double MAX = 10;
 
-    // Constants for angular VPID controller
-    private static double ANGULAR_KP = 0.32;
-    private static double ANGULAR_KI = 0;
-    private static double ANGULAR_KD = 0.6;
-//
-//    // Max/Min angular velocity
-//    public static double MAX_VEL_THETA = 1;
-//    public static double MIN_VEL_THETA = 0.06;
-//
-//    public static double DEADZONE_VEL_THETA = 0.005;
-//
-//    // Constants for angular VPID controller
-//    public static double ANGULAR_KP = -0.55;
-//    public static double ANGULAR_KI = 0;
-//    public static double ANGULAR_KD = -2;
+    // Constants for angular PID controller
+    private static double P = 0.32;
+    private static double I = 0;
+    private static double D = 0.6;
 
     public static double ANGLE_OFFSET = 0; // Degrees offset from center of target
 //    public static double ANGLE_OFFSET = Math.toRadians(5.5); // Degrees offset from center of target
+
+    private static final double THROTTLE_CONSTANT = 3; // Throttle constant for linear velocity
 
     private Executable pipeline;
     private TankDriveTwist2DInput twist2DInput;
@@ -57,10 +48,10 @@ public class PercentManualLineupLocalization extends PIDCommand {
     private final DeepSpaceVisionTargetLocalization deepSpaceVisionTargetLocalization;
 
     public PercentManualLineupLocalization(TankDriveOdometryRunnable driveOdometry, DeepSpaceVisionTargetLocalization deepSpaceVisionTargetLocalization) {
-        super(ANGULAR_KP, ANGULAR_KI, ANGULAR_KD);
+        super(P, I, D);
         this.driveOdometry = driveOdometry;
         this.deepSpaceVisionTargetLocalization = deepSpaceVisionTargetLocalization;
-        System.out.printf("Config updated: P: %f I: %f D: %f Max: %f Min: %f", ANGULAR_KP, ANGULAR_KI, ANGULAR_KD, MAX_VEL_THETA, MIN_VEL_THETA);
+        System.out.printf("Config updated: P: %f I: %f D: %f Max: %f Min: %f", P, I, D, MAX, MIN);
         requires(Robot.drivetrain);
         twist2DInput = new TankDriveTwist2DInput(Tuning.drivetrainRadiusMeters);
         pipeline = twist2DInput
@@ -69,29 +60,29 @@ public class PercentManualLineupLocalization extends PIDCommand {
             .then(Robot.drivetrain.getPipelineOutput(false));
 
         SmartDashboard.putNumber("PercentLineupLocalization/OUTPUT_SCALAR", OUTPUT_SCALAR);
-        SmartDashboard.putNumber("PercentLineupLocalization/ANGULAR_KP", ANGULAR_KP);
-        SmartDashboard.putNumber("PercentLineupLocalization/ANGULAR_KI", ANGULAR_KI);
-        SmartDashboard.putNumber("PercentLineupLocalization/ANGULAR_KD", ANGULAR_KD);
-        SmartDashboard.putNumber("PercentLineupLocalization/MAX_VEL_THETA", MAX_VEL_THETA);
-        SmartDashboard.putNumber("PercentLineupLocalization/MIN_VEL_THETA", MIN_VEL_THETA);
-        SmartDashboard.putNumber("PercentLineupLocalization/DEADZONE_VEL_THETA", DEADZONE_VEL_THETA);
+        SmartDashboard.putNumber("PercentLineupLocalization/ANGULAR_KP", P);
+        SmartDashboard.putNumber("PercentLineupLocalization/ANGULAR_KI", I);
+        SmartDashboard.putNumber("PercentLineupLocalization/ANGULAR_KD", D);
+        SmartDashboard.putNumber("PercentLineupLocalization/MAX_VEL_THETA", MAX);
+        SmartDashboard.putNumber("PercentLineupLocalization/MIN_VEL_THETA", MIN);
+        SmartDashboard.putNumber("PercentLineupLocalization/DEADZONE_VEL_THETA", DEADZONE);
         SmartDashboard.putNumber("PercentLineupLocalization/ANGLE_OFFSET", ANGLE_OFFSET);
     }
 
     @Override
     protected void initialize() {
-        ANGULAR_KP = SmartDashboard.getNumber("PercentLineupLocalization/OUTPUT_SCALAR", OUTPUT_SCALAR);
-        ANGULAR_KP = SmartDashboard.getNumber("PercentLineupLocalization/ANGULAR_KP", ANGULAR_KP);
-        ANGULAR_KI = SmartDashboard.getNumber("PercentLineupLocalization/ANGULAR_KI", ANGULAR_KI);
-        ANGULAR_KD = SmartDashboard.getNumber("PercentLineupLocalization/ANGULAR_KD", ANGULAR_KD);
-        MAX_VEL_THETA = SmartDashboard.getNumber("PercentLineupLocalization/MAX_VEL_THETA", MAX_VEL_THETA);
-        MIN_VEL_THETA = SmartDashboard.getNumber("PercentLineupLocalization/MIN_VEL_THETA", MIN_VEL_THETA);
-        DEADZONE_VEL_THETA = SmartDashboard.getNumber("PercentLineupLocalization/DEADZONE_VEL_THETA", DEADZONE_VEL_THETA);
+        P = SmartDashboard.getNumber("PercentLineupLocalization/OUTPUT_SCALAR", OUTPUT_SCALAR);
+        P = SmartDashboard.getNumber("PercentLineupLocalization/ANGULAR_KP", P);
+        I = SmartDashboard.getNumber("PercentLineupLocalization/ANGULAR_KI", I);
+        D = SmartDashboard.getNumber("PercentLineupLocalization/ANGULAR_KD", D);
+        MAX = SmartDashboard.getNumber("PercentLineupLocalization/MAX_VEL_THETA", MAX);
+        MIN = SmartDashboard.getNumber("PercentLineupLocalization/MIN_VEL_THETA", MIN);
+        DEADZONE = SmartDashboard.getNumber("PercentLineupLocalization/DEADZONE_VEL_THETA", DEADZONE);
         ANGLE_OFFSET = SmartDashboard.getNumber("PercentLineupLocalization/ANGLE_OFFSET", ANGLE_OFFSET);
 
-        this.getPIDController().setP(ANGULAR_KP);
-        this.getPIDController().setI(ANGULAR_KI);
-        this.getPIDController().setD(ANGULAR_KD);
+        this.getPIDController().setP(P);
+        this.getPIDController().setI(I);
+        this.getPIDController().setD(D);
 
         logger.debug("Starting...");
     }
@@ -105,9 +96,9 @@ public class PercentManualLineupLocalization extends PIDCommand {
 
     private double getAngleError() {
         Vector3D odomPosition = driveOdometry.getOdomToBaseLink().getPosition(); // TODO: This should use javaTF
-        double targetAngle = Math.atan2(goal.toTransform2D().getY() - odomPosition.getY(), goal.toTransform2D().getX() - odomPosition.getX());
-        double currentAngle = Hardware.navx.getYawRadians();
-        return TrigUtils.signedAngleError(currentAngle, targetAngle);
+        Vector3D goalPosition = goal.getPosition();
+        double targetAngle = Math.atan2(goalPosition.getY() - odomPosition.getY(), goalPosition.getX() - odomPosition.getX());
+        return TrigUtils.signedAngleError(Hardware.navx.getYawRadians(), targetAngle);
     }
 
     @Override
@@ -125,19 +116,8 @@ public class PercentManualLineupLocalization extends PIDCommand {
 
     @Override
     protected void usePIDOutput(double output) {
-        output *= OUTPUT_SCALAR;
-        double cmdVelTheta = ControlUtils.velocityPosNegConstrain(output, MAX_VEL_THETA, MIN_VEL_THETA);
-        if (Math.abs(output) < DEADZONE_VEL_THETA) {
-            cmdVelTheta = 0;
-        }
-
-        // no ff
-//        Twist2D cmdVel = new Twist2D(OI.getTankdriveLeftAxis() * -0.7, 0, cmdVelTheta);
-//        Robot.drivetrain.setPercentTwist(cmdVel);
-
-        // ff
-        Twist2D cmdVel = new Twist2D(OI.getTankdriveLeftAxis() * -3, 0, -cmdVelTheta);
-        twist2DInput.setTwist(cmdVel);
+        double cmdVelTheta = ControlUtils.allVelocityConstraints(output*OUTPUT_SCALAR, MAX, MIN, DEADZONE);
+        twist2DInput.setTwist(new Twist2D(OI.getPointDriveThrottle() * THROTTLE_CONSTANT, 0, -cmdVelTheta)); // TODO: Figure out why cmdVelTheta is negated
         pipeline.execute();
     }
 
