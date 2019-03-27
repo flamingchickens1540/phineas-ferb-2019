801338209bd8f322c491fc7cb1d888aaf0b4fda5
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-26 20:20:20 -0700

    Finish camera interface

--- src/main/java/org/team1540/robot2019/vision/JonathanCam.java
+++ src/main/java/org/team1540/robot2019/vision/JonathanCam.java
@@ -3,11 +3,16 @@ package org.team1540.robot2019.vision;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import javax.annotation.Nullable;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.apache.log4j.Logger;
 import org.team1540.robot2019.datastructures.threed.Transform3D;
 import org.team1540.robot2019.vision.deepspace.DeepSpaceVisionTargetCamera;
 import org.team1540.robot2019.vision.deepspace.RawDeepSpaceVisionTarget;
 
 public class JonathanCam implements DeepSpaceVisionTargetCamera {
+
+    private static final Logger logger = Logger.getLogger(JonathanCam.class);
 
     private final NetworkTable picamTable;
     private Transform3D baseLinkToCamera;
@@ -37,6 +42,11 @@ public class JonathanCam implements DeepSpaceVisionTargetCamera {
         return null;
     }
 
+    @Override
+    public boolean isTargetFound() {
+        return picamTable.getEntry("vectors").getDoubleArray(new double[]{}).length == 6;
+    }
+
     @Nullable
     @Override
     public RawDeepSpaceVisionTarget getRawDeepSpaceVisionTargetOrNull() {
@@ -45,12 +55,36 @@ public class JonathanCam implements DeepSpaceVisionTargetCamera {
         if (vectors.length != 6) {
             return null;
         }
-        return null;
-//        return new RawDeepSpaceVisionTarget(Math.atan2());
-//
-//        return new RawDeepSpaceVisionTarget(
-//            DualVisionTargetLocalizationUtils.anglesFromScreenSpace(point0, getHorizontalFov(), getVerticalFov()),
-//            DualVisionTargetLocalizationUtils.anglesFromScreenSpace(point1, getHorizontalFov(), getVerticalFov())
-//        );
+
+        Vector3D vectorA = new Vector3D(
+            vectors[0],
+            vectors[1],
+            vectors[2]
+        );
+
+        Vector3D vectorB = new Vector3D(
+            vectors[0],
+            vectors[1],
+            vectors[2]
+        );
+
+        Vector2D anglesA = new Vector2D(
+            Math.atan2(vectorA.getX(), vectorA.getZ()),
+            Math.atan2(vectorA.getY(), vectorA.getZ())
+        );
+
+        Vector2D anglesB = new Vector2D(
+            Math.atan2(vectorB.getX(), vectorB.getZ()),
+            Math.atan2(vectorB.getY(), vectorB.getZ())
+        );
+
+        logger.debug(String.format("A: %f %f B: %f %f",
+            Math.toDegrees(anglesA.getX()),
+            Math.toDegrees(anglesA.getY()),
+            Math.toDegrees(anglesB.getX()),
+            Math.toDegrees(anglesB.getY())
+        ));
+
+        return new RawDeepSpaceVisionTarget(anglesA, anglesB);
     }
 }
