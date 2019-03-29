a1bd8ed0af4fb68304093e3a3955e446a895c4cd
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-29 12:39:33 -0700

    Increase similar vector tracker distance

--- src/main/java/org/team1540/robot2019/commands/auto/PercentManualLineupLocalizationAngleProvider.java
+++ src/main/java/org/team1540/robot2019/commands/auto/PercentManualLineupLocalizationAngleProvider.java
@@ -49,7 +49,7 @@ public class PercentManualLineupLocalizationAngleProvider implements PointAngleP
     private final TankDriveOdometryAccumulatorRunnable driveOdometry;
     private final DeepSpaceVisionTargetLocalization deepSpaceVisionTargetLocalization;
 
-    private final SimilarVector3DTracker similarVectorTracker = new SimilarVector3DTracker(0.1);
+    private final SimilarVector3DTracker similarVectorTracker = new SimilarVector3DTracker(0.2);
     private Timer timer;
 
     public PercentManualLineupLocalizationAngleProvider(TankDriveOdometryAccumulatorRunnable driveOdometry, DeepSpaceVisionTargetLocalization deepSpaceVisionTargetLocalization) {
