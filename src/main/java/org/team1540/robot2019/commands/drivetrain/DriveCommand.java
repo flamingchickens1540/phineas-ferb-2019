304bd36f7828295de23ea23d33ef2b87d3243802
wangl5 <wangl5@users.noreply.github.com>
2019-03-28 19:56:57 -0700

    Disable align when elevator is up

--- src/main/java/org/team1540/robot2019/commands/drivetrain/DriveCommand.java
+++ src/main/java/org/team1540/robot2019/commands/drivetrain/DriveCommand.java
@@ -43,10 +43,10 @@ public class DriveCommand extends PointManualDriveCommand { // TODO: Make this g
             tempDisableLineup = false;
             startPointDrive();
         } else {
-            if (!tempDisableLineup) {
-                startLineup();
-            } else {
+            if (tempDisableLineup || Robot.elevator.getPosition() > 3) { // TODO: This should be a tuning constant
                 return 0;
+            } else {
+                startLineup();
             }
         }
         return currentAngleProvider.returnAngleError();
