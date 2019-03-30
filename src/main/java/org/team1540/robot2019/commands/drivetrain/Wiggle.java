e1a8848d34c2905e236484021d69e108020d66a8
wangl5 <wangl5@users.noreply.github.com>
2019-03-29 17:55:49 -0700

    Remove last drive from wiggle command

--- src/main/java/org/team1540/robot2019/commands/drivetrain/Wiggle.java
+++ src/main/java/org/team1540/robot2019/commands/drivetrain/Wiggle.java
@@ -8,11 +8,10 @@ public class Wiggle extends CommandGroup { // do do do, doo, doo, doo
     private static final double SLOW_PERCENT = -0.05;
     private static final double FAST_PERCENT = 0.2;
 
-    public static final double TOTAL_TIME = DRIVE_TIME * 4;
+    public static final double TOTAL_TIME = DRIVE_TIME * 3;
 
     public Wiggle() {
         addSequential(new TankDriveForTimePercent(DRIVE_TIME, SLOW_PERCENT, FAST_PERCENT));
         addSequential(new TankDriveForTimePercent(DRIVE_TIME * 2, FAST_PERCENT * 2, SLOW_PERCENT * 2));
-        addSequential(new TankDriveForTimePercent(DRIVE_TIME, SLOW_PERCENT, FAST_PERCENT));
     }
 }
