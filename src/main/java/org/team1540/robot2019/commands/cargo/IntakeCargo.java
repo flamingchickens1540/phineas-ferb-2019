7e2aaacf9a2e910481fdd1aab3141ca41c52eefe
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-29 17:55:08 -0700

    Add current limit intake end condition for ferb

--- src/main/java/org/team1540/robot2019/commands/cargo/IntakeCargo.java
+++ src/main/java/org/team1540/robot2019/commands/cargo/IntakeCargo.java
@@ -1,20 +1,26 @@
 package org.team1540.robot2019.commands.cargo;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.apache.log4j.Logger;
+import org.team1540.robot2019.Hardware;
 import org.team1540.robot2019.Robot;
+import org.team1540.robot2019.Tuning;
 
 public class IntakeCargo extends Command {
 
     private static final Logger logger = Logger.getLogger(IntakeCargo.class);
+    private static double MAX_CURRENT = 8;
 
     public IntakeCargo() {
         requires(Robot.intake);
+        SmartDashboard.putNumber("Intake/MAX_CURRENT", MAX_CURRENT);
     }
 
     @Override
     protected void initialize() {
-        logger.debug("Intake starting");
+        MAX_CURRENT = SmartDashboard.getNumber("Intake/MAX_CURRENT", MAX_CURRENT);
+        logger.debug("Intake starting, MAX_CURRENT: " + MAX_CURRENT);
         Robot.intake.startIntaking();
     }
 
@@ -26,6 +32,10 @@ public class IntakeCargo extends Command {
 
     @Override
     protected boolean isFinished() {
+        SmartDashboard.putNumber("PDP 6", Hardware.pdp.getCurrent(6));
+        if (!Tuning.isComp && this.timeSinceInitialized() > 0.5 && Hardware.pdp.getCurrent(6) > MAX_CURRENT) {
+            return true;
+        }
         return Robot.intake.hasBall() || isTimedOut();
     }
 }
