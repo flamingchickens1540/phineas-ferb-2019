b04a81cbea69911ad22f74b8af205c8e4368eefa
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-03-29 17:54:13 -0700

    Add debugging to wrist commands

--- src/main/java/org/team1540/robot2019/commands/wrist/WristUp.java
+++ src/main/java/org/team1540/robot2019/commands/wrist/WristUp.java
@@ -1,19 +1,24 @@
 package org.team1540.robot2019.commands.wrist;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.apache.log4j.Logger;
 import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.Tuning;
 
 public class WristUp extends Command {
 
+    private static final Logger logger = Logger.getLogger(WristUp.class);
+
     @Override
     protected void initialize() {
+        logger.debug("Moving wrist up...");
         Robot.wrist.clearMidFlag();
         Robot.wrist.set(-Tuning.wristUpTravelThrot);
     }
 
     @Override
     protected void end() {
+        logger.debug("Mid flag reached! Holding...");
         Robot.wrist.set(-Tuning.wristHoldThrot);
     }
 
