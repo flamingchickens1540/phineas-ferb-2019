7dd7125758b6affa7c0336293513a337f51d7b5e
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-04-01 12:49:50 -0700

    Add WaitUntilCommand

--- src/main/java/org/team1540/robot2019/commands/auto/AutoPlaceSequence.java
+++ src/main/java/org/team1540/robot2019/commands/auto/AutoPlaceSequence.java
@@ -1,6 +1,5 @@
 package org.team1540.robot2019.commands.auto;
 
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.commands.drivetrain.AutoLineupAndDrive;
 import org.team1540.robot2019.commands.drivetrain.TankDriveForTimePercent;
@@ -10,16 +9,7 @@ public class AutoPlaceSequence extends CommandGroup {
 
     public AutoPlaceSequence() {
         AutoLineupAndDrive autoLineupAndDrive = new AutoLineupAndDrive();
-        addParallel(new Command() {
-            @Override
-            protected boolean isFinished() {
-                if (autoLineupAndDrive.getDistanceToVisionTarget() < 0.5) {
-                    autoLineupAndDrive.enableEndFlag();
-                    return true;
-                }
-                return false;
-            }
-        });
+        addParallel(new WaitUntilCommand(() -> autoLineupAndDrive.getDistanceToVisionTarget() < 0.5, autoLineupAndDrive::enableEndFlag));
         addSequential(autoLineupAndDrive);
         addSequential(new AutoPlaceHatchSequence());
         addSequential(new TankDriveForTimePercent(1.5, -0.2));
