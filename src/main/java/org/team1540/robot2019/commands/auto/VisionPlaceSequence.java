932ef0020d3eea9e2b3c448c5dca6ae406c40285
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-04-02 21:19:19 -0700

    Edit drive and vision grab and place sequences

---
+++ src/main/java/org/team1540/robot2019/commands/auto/VisionPlaceSequence.java
@@ -0,0 +1,16 @@
+package org.team1540.robot2019.commands.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.team1540.robot2019.Robot;
+import org.team1540.robot2019.commands.hatch.AutoPlaceHatchSequenceNoDTReq;
+
+public class VisionPlaceSequence extends CommandGroup {
+
+    public VisionPlaceSequence() {
+        addSequential(new WaitUntilCommand(() -> {
+            return Robot.drivetrain.getDriveCommand().getLineupLocalization().getDistanceToVisionTarget() < 0.4
+                && Robot.drivetrain.getDriveCommand().getLineupLocalization().returnAngleError() < Math.toRadians(3);
+        }));
+        addSequential(new AutoPlaceHatchSequenceNoDTReq());
+    }
+}
