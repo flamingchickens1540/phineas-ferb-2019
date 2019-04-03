439491219280807d21f5059bc046b2196b5b0b88
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-04-02 21:18:23 -0700

    Edit grab and place sequences

---
+++ src/main/java/org/team1540/robot2019/commands/hatch/AutoGrabHatchSequence.java
@@ -0,0 +1,30 @@
+package org.team1540.robot2019.commands.hatch;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.team1540.robot2019.Tuning;
+import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
+import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
+import org.team1540.robot2019.commands.hatch.simple.ExtendHatchMech;
+import org.team1540.robot2019.commands.hatch.simple.GrabHatch;
+import org.team1540.robot2019.commands.hatch.simple.ReleaseHatch;
+import org.team1540.robot2019.commands.hatch.simple.RetractHatchMech;
+
+public class AutoGrabHatchSequence extends CommandGroup {
+
+    public AutoGrabHatchSequence() {
+        this(null);
+    }
+
+    public AutoGrabHatchSequence(Runnable onSensorTrip) {
+        addParallel(new MoveElevatorToPosition(Tuning.elevatorHatchIntakePosition));
+        addSequential(new ReleaseHatch());
+        addSequential(new ExtendHatchMech());
+        addSequential(new WaitCommand(0.2));
+        addSequential(new GrabHatch());
+        addSequential(new WaitCommand(0.5));
+        addSequential(new RetractHatchMech());
+        addSequential(new WaitCommand(0.1));
+        addSequential(new MoveElevatorToZero());
+    }
+}
