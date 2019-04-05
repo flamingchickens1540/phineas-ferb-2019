186ddc6bac6bbd11c14ba21979570e2043300153
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-04-04 18:02:40 -0700

    Make grab hatch sequence cancel floor intake

--- src/main/java/org/team1540/robot2019/commands/hatch/SensorGrabHatchSequence.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/SensorGrabHatchSequence.java
@@ -2,6 +2,7 @@ package org.team1540.robot2019.commands.hatch;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.Tuning;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
@@ -18,6 +19,7 @@ public class SensorGrabHatchSequence extends CommandGroup {
     }
 
     public SensorGrabHatchSequence(Runnable onSensorTrip) {
+        requires(Robot.wrist);
         addParallel(new MoveElevatorToPosition(Tuning.elevatorHatchIntakePosition));
         addSequential(new ReleaseHatch());
         addSequential(new ExtendHatchMech());
@@ -33,5 +35,10 @@ public class SensorGrabHatchSequence extends CommandGroup {
         addSequential(new RetractHatchMech());
         addSequential(new WaitCommand(0.1));
         addSequential(new MoveElevatorToZero());
+    }
+
+    @Override
+    protected void interrupted() {
+        new RetractHatchMech().start();
     }
 }
