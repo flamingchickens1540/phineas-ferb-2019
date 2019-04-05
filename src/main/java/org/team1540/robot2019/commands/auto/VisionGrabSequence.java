9e16309d872ed5fe646f923605591a7f15f8ce07
wangl5 <wangl5@users.noreply.github.com>
2019-04-04 17:26:58 -0700

    Add constants for vision grab and place

--- src/main/java/org/team1540/robot2019/commands/auto/VisionGrabSequence.java
+++ src/main/java/org/team1540/robot2019/commands/auto/VisionGrabSequence.java
@@ -6,11 +6,13 @@ import org.team1540.robot2019.commands.hatch.AutoGrabHatchSequence;
 
 public class VisionGrabSequence extends CommandGroup {
 
+    public static final double MAX_DISTANCE = 0.35;
+    public static final double MAX_ANGLE_ERROR = Math.toRadians(3);
+
     public VisionGrabSequence() {
-        addSequential(new WaitUntilCommand(() -> {
-            return Robot.drivetrain.getDriveCommand().getLineupLocalization().getDistanceToVisionTarget() < 0.35
-                && Robot.drivetrain.getDriveCommand().getLineupLocalization().returnAngleError(0) < Math.toRadians(3);
-        }));
+        addSequential(new WaitUntilCommand(() ->
+            Robot.drivetrain.getDriveCommand().getLineupLocalization().getDistanceToVisionTarget() < MAX_DISTANCE
+                && Robot.drivetrain.getDriveCommand().getLineupLocalization().returnAngleError(0) < MAX_ANGLE_ERROR));
         addSequential(new AutoGrabHatchSequence());
     }
 }
