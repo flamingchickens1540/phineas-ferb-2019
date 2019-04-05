31a7122324d26580678365d189fd4a5a36f71dd1
wangl5 <wangl5@users.noreply.github.com>
2019-04-05 14:47:21 -0700

    Make climb angle and arm pos tunable

--- src/main/java/org/team1540/robot2019/commands/climber/PrepClimbLevelTwo.java
+++ src/main/java/org/team1540/robot2019/commands/climber/PrepClimbLevelTwo.java
@@ -1,19 +1,31 @@
 package org.team1540.robot2019.commands.climber;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.team1540.robot2019.Tuning;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
 
 public class PrepClimbLevelTwo extends CommandGroup {
 
+    private static double ARM_POS = Tuning.climberStartPosLevel2;
     public static boolean hasPrepLvl2 = false;
+    private MoveArmsToPosition command;
 
     public PrepClimbLevelTwo() {
+        SmartDashboard.putNumber("Level2ArmPos", ARM_POS);
+
         addSequential(new MoveElevatorToPosition(Tuning.elevatorClimbPosition));
-        addSequential(new MoveArmsToPosition(Tuning.climberStartPosLevel2));
+        command = new MoveArmsToPosition(Tuning.climberStartPosLevel2);
+        addSequential(command);
         addSequential(new MoveElevatorToZero());
 //        addSequential(new LiftGyroStabilizeLevel2());
+    }
+
+    @Override
+    protected void initialize() {
+        ARM_POS = SmartDashboard.getNumber("Level2ArmPos", ARM_POS);
+        command.setPosition(ARM_POS);
     }
 
     @Override
