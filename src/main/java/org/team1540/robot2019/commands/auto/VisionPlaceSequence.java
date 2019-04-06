9144903780a1571525b89e34cb939772c81ff11e
wangl5 <wangl5@users.noreply.github.com>
2019-04-06 07:02:15 -0700

    Add debug

--- src/main/java/org/team1540/robot2019/commands/auto/VisionPlaceSequence.java
+++ src/main/java/org/team1540/robot2019/commands/auto/VisionPlaceSequence.java
@@ -1,10 +1,13 @@
 package org.team1540.robot2019.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.apache.log4j.Logger;
 import org.team1540.robot2019.Robot;
 import org.team1540.robot2019.commands.hatch.AutoPlaceHatchSequenceNoDTReq;
 
 public class VisionPlaceSequence extends CommandGroup {
+
+    private static final Logger logger = Logger.getLogger(VisionPlaceSequence.class);
 
     public static final double MAX_DISTANCE = 0.4;
     public static final double MAX_ANGLE_ERROR = Math.toRadians(3);
@@ -14,5 +17,15 @@ public class VisionPlaceSequence extends CommandGroup {
             Robot.drivetrain.getDriveCommand().getLineupLocalization().getDistanceToVisionTarget() < MAX_DISTANCE
                 && Robot.drivetrain.getDriveCommand().getLineupLocalization().returnAngleError(0) < MAX_ANGLE_ERROR));
         addSequential(new AutoPlaceHatchSequenceNoDTReq());
+    }
+
+    @Override
+    protected void initialize() {
+        logger.debug("Init");
+    }
+
+    @Override
+    protected void end() {
+        logger.debug("End");
     }
 }
