f9196006b2c32c85032c98f37db3bb42bd3db480
wangl5 <wangl5@users.noreply.github.com>
2019-04-10 17:49:19 -0700

    Remove auto grab hatch sequence

--- src/main/java/org/team1540/robot2019/commands/hatch/AutoGrabHatchSequence.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/AutoGrabHatchSequence.java
@@ -1,14 +1,6 @@
 package org.team1540.robot2019.commands.hatch;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.WaitCommand;
-import org.team1540.robot2019.Tuning;
-import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
-import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
-import org.team1540.robot2019.commands.hatch.simple.ExtendHatchMech;
-import org.team1540.robot2019.commands.hatch.simple.GrabHatch;
-import org.team1540.robot2019.commands.hatch.simple.ReleaseHatch;
-import org.team1540.robot2019.commands.hatch.simple.RetractHatchMech;
 
 public class AutoGrabHatchSequence extends CommandGroup {
 
@@ -17,14 +9,15 @@ public class AutoGrabHatchSequence extends CommandGroup {
     }
 
     public AutoGrabHatchSequence(Runnable onSensorTrip) {
-        addParallel(new MoveElevatorToPosition(Tuning.elevatorHatchIntakePosition));
-        addSequential(new ReleaseHatch());
-        addSequential(new ExtendHatchMech());
-        addSequential(new WaitCommand(0.2));
-        addSequential(new GrabHatch());
-        addSequential(new WaitCommand(0.5));
-        addSequential(new RetractHatchMech());
-        addSequential(new WaitCommand(0.1));
-        addSequential(new MoveElevatorToZero());
+        // this is outdated
+//        addParallel(new MoveElevatorToPosition(Tuning.elevatorHatchIntakePosition));
+//        addSequential(new ReleaseHatch());
+//        addSequential(new ExtendHatchMech());
+//        addSequential(new WaitCommand(0.2));
+//        addSequential(new GrabHatch());
+//        addSequential(new WaitCommand(0.5));
+//        addSequential(new RetractHatchMech());
+//        addSequential(new WaitCommand(0.1));
+//        addSequential(new MoveElevatorToZero());
     }
 }
