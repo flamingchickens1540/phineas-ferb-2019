52fef9215840014cb5464ce9589efa7464c61335
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-04-10 17:49:43 -0700

    Speed up place hatch sequence

--- src/main/java/org/team1540/robot2019/commands/hatch/PlaceHatchSequence.java
+++ src/main/java/org/team1540/robot2019/commands/hatch/PlaceHatchSequence.java
@@ -1,7 +1,6 @@
 package org.team1540.robot2019.commands.hatch;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.TimedCommand;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.team1540.robot2019.commands.drivetrain.TankDriveForTimePercent;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
@@ -9,18 +8,20 @@ import org.team1540.robot2019.commands.hatch.simple.ExtendHatchMech;
 import org.team1540.robot2019.commands.hatch.simple.GrabHatch;
 import org.team1540.robot2019.commands.hatch.simple.ReleaseHatch;
 import org.team1540.robot2019.commands.hatch.simple.RetractHatchMech;
+import org.team1540.robot2019.commands.wrist.WristUp;
 import org.team1540.rooster.util.SimpleCommand;
 
 public class PlaceHatchSequence extends CommandGroup {
 
     public PlaceHatchSequence() {
+        addSequential(new WristUp());
+        addSequential(new SimpleCommand("Drive", () -> new TankDriveForTimePercent(0.2, 0.35).start()));
+        addSequential(new WaitCommand(0.1));
         addSequential(new GrabHatch());
         addSequential(new ExtendHatchMech());
-        addSequential(new SimpleCommand("Drive", () -> new TankDriveForTimePercent(0.3, 0.3).start()));
-        addSequential(new TimedCommand(0.3));
         addSequential(new WaitCommand(0.1));
         addSequential(new ReleaseHatch());
-        addSequential(new WaitCommand(0.1));
+        addSequential(new WaitCommand(0.2));
         addSequential(new RetractHatchMech());
         addSequential(new WaitCommand(0.1));
         addSequential(new MoveElevatorToZero());
