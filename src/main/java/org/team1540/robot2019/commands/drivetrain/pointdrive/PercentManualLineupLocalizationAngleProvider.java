2a1ba837ef5ef497dccec7c501252940d2ac546a
wangl5 <wangl5@users.noreply.github.com>
2019-04-17 23:29:18 -0500

    Tune place position

--- src/main/java/org/team1540/robot2019/commands/drivetrain/pointdrive/PercentManualLineupLocalizationAngleProvider.java
+++ src/main/java/org/team1540/robot2019/commands/drivetrain/pointdrive/PercentManualLineupLocalizationAngleProvider.java
@@ -31,9 +31,9 @@ public class PercentManualLineupLocalizationAngleProvider implements PointAngleP
 
     static {
         if (Tuning.isComp) {
-            BALL_CARGOSHIP_PLACE_X_OFFSET = -0.1;
+            BALL_CARGOSHIP_PLACE_X_OFFSET = 0.1;
             BALL_CARGOSHIP_PLACE_Y_OFFSET = 0.003;
-            BALL_ROCKET_PLACE_Y_OFFSET = 0.003;
+            BALL_ROCKET_PLACE_Y_OFFSET = -0.03;
             BALL_ROCKET_PLACE_X_OFFSET = -0.1;
             HATCH_GRAB_X_OFFSET = -0.05;
             HATCH_GRAB_Y_OFFSET = 0.01;
