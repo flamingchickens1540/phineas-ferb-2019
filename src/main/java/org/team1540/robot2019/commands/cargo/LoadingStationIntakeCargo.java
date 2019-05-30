8e64b7156e2344c38fa04f9560b8133af3f17b4d
Zachary Robinson <robinsonz@catlin.edu>
2019-05-30 08:33:28 -0700

    Replace one more StowHatchMech

--- src/main/java/org/team1540/robot2019/commands/cargo/LoadingStationIntakeCargo.java
+++ src/main/java/org/team1540/robot2019/commands/cargo/LoadingStationIntakeCargo.java
@@ -4,15 +4,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.team1540.robot2019.Tuning;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
 import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
-import org.team1540.robot2019.commands.hatch.simple.ReleaseHatch;
-import org.team1540.robot2019.commands.hatch.simple.RetractHatchMech;
+import org.team1540.robot2019.commands.hatch.subgroups.StowHatchMech;
 import org.team1540.robot2019.commands.wrist.WristUp;
 
 public class LoadingStationIntakeCargo extends CommandGroup {
 
     public LoadingStationIntakeCargo() {
-        addSequential(new ReleaseHatch());
-        addSequential(new RetractHatchMech());
+        addSequential(new StowHatchMech());
         addSequential(new WristUp());
         addParallel(new MoveElevatorToPosition(Tuning.elevatorLoadingStationPosition));
         addSequential(new IntakeCargo());
