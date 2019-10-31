48ef6fe1c2ea013f048177c57ee1791719a12745
Zachary Robinson <robinsonz@catlin.edu>
2019-10-31 09:24:41 -0700

    Don't move elevator during loading station intake

--- src/main/java/org/team1540/robot2019/commands/cargo/LoadingStationIntakeCargo.java
+++ src/main/java/org/team1540/robot2019/commands/cargo/LoadingStationIntakeCargo.java
@@ -1,9 +1,6 @@
 package org.team1540.robot2019.commands.cargo;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import org.team1540.robot2019.Tuning;
-import org.team1540.robot2019.commands.elevator.MoveElevatorToPosition;
-import org.team1540.robot2019.commands.elevator.MoveElevatorToZero;
 import org.team1540.robot2019.commands.hatch.simple.ReleaseHatch;
 import org.team1540.robot2019.commands.hatch.simple.RetractHatchMech;
 import org.team1540.robot2019.commands.wrist.WristUp;
@@ -14,8 +11,8 @@ public class LoadingStationIntakeCargo extends CommandGroup {
         addSequential(new ReleaseHatch());
         addSequential(new RetractHatchMech());
         addSequential(new WristUp());
-        addParallel(new MoveElevatorToPosition(Tuning.elevatorLoadingStationPosition));
+//        addParallel(new MoveElevatorToPosition(Tuning.elevatorLoadingStationPosition));
         addSequential(new IntakeCargo());
-        addSequential(new MoveElevatorToZero());
+//        addSequential(new MoveElevatorToZero());
     }
 }
